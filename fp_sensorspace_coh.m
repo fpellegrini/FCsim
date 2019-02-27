9c07eb45698d34099524ba71300c52e41dc4c3b3
Franzi <franziskapellegrini@web.de>
2019-02-27 13:44:27 +0100

    modified fp_sensorspace_coh. Now it plots also imaginary parts of the coherence. Included a subfunction for plotting.

--- fp_sensorspace_coh.m
+++ fp_sensorspace_coh.m
@@ -37,10 +37,11 @@ for id = 1:numel(patientID)
     conn = data2spwctrgc(data, fres, nlags, cond, nboot, [], {'CS'});        
     COH = cs2coh(conn.CS);
     absCOH = abs(COH(:, meg_inds, lfp_inds));
+    imCOH = imag(COH(:, meg_inds, lfp_inds));
     
     loc = mk_sensors_plane(D_ft.grad.chanpos(:, [2 1 3]));
-    close all
     frq_inds = find(frqs > 15 & frqs < 23);
+
     
 %     figone(17,38)
 %     for i = 1:6
@@ -54,75 +55,71 @@ for id = 1:numel(patientID)
 %     print(outname,'-dpng');
 %     close all
 
-    %right lfp channels
-    
+    %right lfp channels   
     r_COH = mean(absCOH(frq_inds,:,1:3),3); %15 x 125
-    temp = max(max(r_COH));
-    [r_max_f r_max_chan] = find(r_COH==temp);
-    clear temp
-    
-    figone(30,30)
-    pars.scale = [0 0.15];
-    %plot right channels
-    %mean across freqs between 15 and 22
-    subplot(2,2,1)
-    showfield_general(mean(r_COH,1)', loc, pars); 
-    caxis([0 0.15])
-    text(-0.5, 0.7,'mean coherence across frequencies (15-22 Hz)')
-    
-    %only for the strongest frequency
-    subplot(2,2,2)
-    showfield_general(r_COH(r_max_f, :)', loc, pars);
-    text(-0.4, 0.7,'coherency at strongest frequency')
-    
-    %coherence spectrum at strongest channel
-    subplot(2,2,3)
-    plot(frqs(frq_inds), r_COH(:,r_max_chan))
-    xlabel('Freq (Hz)')
-    ylabel('Coherence')
-    title('Coherence spectrum at strongest channel') 
-    
     clear outname
-    outname = sprintf('%scoh_analysis_Patient%s_right.png',DIROUT,patientID{id});
-    print(outname,'-dpng');
-    close all
+    outname = sprintf('%scoh_abs_Patient%s_right.png',DIROUT,patientID{id});
     
+    fp_plot_sensorspace_coh(r_COH,frqs, frq_inds, loc, [0 0.15] ,outname)
     
-    %left lfp channels
-    
+    %left lfp channels 
     l_COH = mean(absCOH(frq_inds,:,4:6),3); %15 x 125
-    temp = max(max(l_COH));
-    [l_max_f l_max_chan] = find(l_COH==temp);
-    clear temp
-    
-    figone(30,30)
-    pars.scale = [0 0.15];
-    %plot right channels
-    %mean across freqs between 15 and 22
-    subplot(2,2,1)
-    showfield_general(mean(l_COH,1)', loc, pars); 
-    caxis([0 0.15])
-    text(-0.5, 0.7,'mean coherence across frequencies (15-22 Hz)')
-    
-    %only for the strongest frequency
-    subplot(2,2,2)
-    showfield_general(l_COH(l_max_f, :)', loc, pars);
-    text(-0.4, 0.7,'coherency at strongest frequency')
-    
-    %coherence spectrum at strongest channel
-    subplot(2,2,3)
-    plot(frqs(frq_inds), l_COH(:,l_max_chan))
-    xlabel('Freq (Hz)')
-    ylabel('Coherence')
-    title('Coherence spectrum at strongest channel') 
-    
     clear outname
-    outname = sprintf('%scoh_analysis_Patient%s_left.png',DIROUT,patientID{id});
-    print(outname,'-dpng');
-    close all
+    outname = sprintf('%scoh_abs_Patient%s_left.png',DIROUT,patientID{id});
     
+    fp_plot_sensorspace_coh(l_COH,frqs,frq_inds, loc, [0 0.15],outname)
     
-    clear fileName D D_ft data conn COH absCOH loc l_COH r_COH
+    %plot imaginary parts 
+    %right
+    rim_COH = mean(imCOH(frq_inds,:,1:3),3); %15 x 125
+    clear outname
+    outname = sprintf('%scoh_imag_Patient%s_right.png',DIROUT,patientID{id});
+        
+    fp_plot_sensorspace_coh(rim_COH,frqs, frq_inds, loc, [-0.15 0.15],outname)
+    
+    %left
+    lim_COH = mean(imCOH(frq_inds,:,4:6),3); %15 x 125
+    clear outname
+    outname = sprintf('%scoh_imag_Patient%s_left.png',DIROUT,patientID{id});
+        
+    fp_plot_sensorspace_coh(lim_COH,frqs, frq_inds, loc,[-0.15 0.15],outname)
+    
+    clear fileName D D_ft data conn COH absCOH imCOH loc l_COH r_COH lim_COH rim_COH
     
     
 end
+end 
+
+
+function fp_plot_sensorspace_coh(coh,frqs, frq_inds, loc,c_scale, outname)
+    
+    
+    temp = max(max(coh));
+    [max_f, max_chan] = find(coh==temp);
+    clear temp
+    
+    figone(30,30)
+    pars.scale = c_scale;
+    %plot right channels
+    %mean across freqs between 15 and 22
+    subplot(2,2,1)
+    showfield_general(mean(coh,1)', loc, pars); 
+    caxis(c_scale)
+    text(-0.5, 0.7,'Mean coherence across frequencies (15-22 Hz)')
+    
+    %only for the strongest frequency
+    subplot(2,2,2)
+    showfield_general(coh(max_f, :)', loc, pars);
+    text(-0.4, 0.7,'Coherence at strongest frequency')
+    
+    %coherence spectrum at strongest channel
+    subplot(2,2,3)
+    plot(frqs(frq_inds), coh(:,max_chan))
+    xlabel('Freq (Hz)')
+    ylabel('Coherence')
+    title('Coherence spectrum at strongest channel') 
+    
+    print(outname,'-dpng');
+    close all
+end 
+    
