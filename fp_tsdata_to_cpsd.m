30ec3298b4f6ca127d86d205265700d6a7f38bab
Franzi <franziskapellegrini@web.de>
2019-02-27 17:33:29 +0100

    Fixed a bug and modified it: now you specify whether you want to calculate the 'whole' cs or only the cs between meg and lfp channels.

--- fp_tsdata_to_cpsd.m
+++ fp_tsdata_to_cpsd.m
@@ -1,12 +1,13 @@
-function S = fp_tsdata_to_cpsd(X,S,fres,id_meg_chan, id_lfp_chan, id_meg_trials, id_lfp_trials, window,noverlap)
+function S = fp_tsdata_to_cpsd(X,wholeCS,fres,id_meg_chan, id_lfp_chan, id_meg_trials, id_lfp_trials, window,noverlap)
 
 % Estimate cross-power spectral density from time series data.
-% If S is empty, the whole cross spectrum will be calculated. If not, only
+% If wholeCS is true, the whole cross spectrum will be calculated. If not, only
 % the cross spectrum between meg channels and the 6 lfp channels will be
-% calculated and replaced in S. 
+% calculated. 
 % Shuffle id_lfp_trials for surrogate images. 
 
-
+MEG_CHAN = 1:125;
+LFP_CHAN = 126:131;
 [~,m,N] = size(X); %n channels, m time points, N trials 
 n = numel(id_meg_chan) + numel(id_lfp_chan);
 X = demean(X);
@@ -24,12 +25,12 @@ if ~exist('overlap','var')
 end
 assert(noverlap < window,'overlap must be shorter than window');
 
-if isempty(S)
+if wholeCS==1
     S = 0;
 
     for r = 1:numel(id_meg_trials)
-        x_meg = X(:,id_meg_chan,id_meg_trials(r)); %running trial for meg 
-        x_lfp = X(:,id_lfp_chan,id_lfp_trials(r)); %running trial for lfp (potentially permuted)
+        x_meg = X(:,MEG_CHAN,id_meg_trials(r)); %running trial for meg 
+        x_lfp = X(:,LFP_CHAN,id_lfp_trials(r)); %running trial for lfp (potentially permuted)
         x_current = cat(2,x_meg,x_lfp);
         XX = fp_cpsd_welch(x_current,1:size(x_current,2),1:size(x_current,2),fres+1,window,noverlap);
         S = S + XX; 
@@ -44,19 +45,17 @@ if isempty(S)
     end
     
 else
-    temp_S = 0;
+    S = 0;
     for r = 1:N
-        x_meg = X(:,id_meg_chan,id_meg_trials(r)); %running trial for meg 
-        x_lfp = X(:,id_lfp_chan,id_lfp_trials(r)); %running trial for lfp (potentially permuted)
+        x_meg = X(:,MEG_CHAN,id_meg_trials(r)); %running trial for meg 
+        x_lfp = X(:,LFP_CHAN,id_lfp_trials(r)); %running trial for lfp (potentially permuted)
         x_current = cat(2,x_meg,x_lfp);
         XX = fp_cpsd_welch(x_current,id_meg_chan,id_lfp_chan,fres+1,window,noverlap);
-        temp_S = temp_S + XX; 
+        S = S + XX; 
 
     end
-    temp_S = temp_S/N; % average across trials; formerly: S = pi*S/N;
-    
-    S(id_meg_chan,id_lfp_chan,:) = temp_S;
-    S(id_lfp_chan,id_meg_chan,:) = permute(temp_S,[2 1 3]);
+    S = S/N; % average across trials; formerly: S = pi*S/N;
+   
 
 end
     
