7592d6d07bb4e131eff30d4d5d36c7ebb5c77cb8
“[franziska <franziskapellegrini@web.de>
2019-04-09 18:23:36 +0200

    modified group cluster fun, fp_find_commonvox is used in that fun.

--- fp_cluster_group.m
+++ fp_cluster_group.m
@@ -14,16 +14,12 @@ if isempty(abs_imag)
     abs_imag = 'abs';
 end 
 
-if strcmp(abs_imag,'abs')
-    threshold= 0.0698; %mean of singlesub thresholds 
-else 
-    threshold = 0.0549; 
-end
+[commonvox_pos, voxID] = fp_find_commonvox;
 
 nsubs = numel(patientID);
 nit = 51;
 nfreq = 46;
-ns = 1917; %this is the issue
+ns = size(commonvox_pos,1); 
 fs = 300;
 fres = 75;
 frqs = sfreqs(fres, fs);
@@ -31,21 +27,26 @@ frqs(frqs>90) = [];
 
 COH = nan(nsubs,nit,nfreq,ns);
 
+
+
 for id = 1:nsubs
     
     %load coherences
     load(sprintf('Coherences_Patient%s.mat',patientID{id}));
     mni_pos = fp_getMNIpos(patientID{id});
     conn = fp_find_neighbours(patientID{id}); %think about how to do that 
+    match_conn = conn(voxID{id},voxID{id});
     
     %get flip id and symmetric head
     [sym_pos, noEq] = fp_symmetric_vol(mni_pos);
-    [~,flip_id] = fp_flip_vol(sym_pos);
+    match_pos = sym_pos(voxID{id},:);
+    [~,flip_id] = fp_flip_vol(match_pos);
     
     %flip coherence
     coh(:,:,noEq,:) = [];
-    flip_coh = coh;
-    flip_coh(:,:,:,4:6) = coh(:,:,flip_id,4:6);
+    match_coh = coh(:,:,voxID{id},:);
+    flip_coh = match_coh;
+    flip_coh(:,:,:,4:6) = match_coh(:,:,flip_id,4:6);
     
     %absolute value
     if strcmp(abs_imag,'abs')
@@ -61,20 +62,31 @@ for id = 1:nsubs
 
 end
 
-avg_coh = squeeze(sum(COH,1));
+avg_coh = squeeze(median(sum(COH(:,:,13:30,:),1),3));
+threshold = prctile(reshape(avg_coh,1,[]),99);
 onoff = avg_coh>threshold;
 
-big_clusters = zeros(nit,nfreq,ns);
+% big_clusters = zeros(nit,nfreq,ns);
+big_clusters = zeros(nit,ns);
 
 for iit = 1: nit
 
     clear cluster total x big_clu_id
-    [cluster, total] = findcluster(squeeze(onoff(iit,:,:))',conn,conn, minnbchan); %try components fun
+    [clu, total] = findcluster(squeeze(onoff(iit,:))',match_conn,match_conn, minnbchan);
 
     if total>0
-        x = hist(cluster(:),0:total);
-        big_clu_id = find(max(x));
-        big_clusters(iit,:,:) = cluster == big_clu_id;
+        x = hist(clu(:),0:total);
+        big_clu_id = find(x(2:end)==max(x(2:end)));
+        big_clusters(iit,:,:) = clu == big_clu_id;
+        
+%         figure
+%         c=commonvox_pos;
+%         clear mask
+%         mask = big_clusters(iit,:);
+%         scatter3(c(:,1),c(:,2),c(:,3),5,[0.85 0.85 0.85])
+%         hold on
+%         scatter3(c(mask==1,1),c(mask==1,2),c(mask==1,3),20,[0.8 0.1,0.5],'filled')
+%         colormap jet
     end
 
 end
---
+++ fp_find_commonvox.m
@@ -0,0 +1,51 @@
+function [commonvox_pos, voxID] = fp_find_commonvox
+%finds voxels that exist in all subjects 
+%index according to sym_pos, not according to original mni_pos! 
+
+cd ~/Dropbox/Data_MEG_Project/
+
+patientID = {'04'; '07'; '08'; '09'; '10';'11';'12';'18';'20';'22';'25'};
+
+clear mni_pos sym_pos
+mni_pos = fp_getMNIpos(patientID{1});
+[sym_pos, ~] = fp_symmetric_vol(mni_pos);
+commonvox_pos = sym_pos;
+
+for id = 2:numel(patientID)
+   
+    clear mni_pos sym_pos
+    mni_pos = fp_getMNIpos(patientID{id});    
+    [sym_pos, ~] = fp_symmetric_vol(mni_pos);
+    clear b
+    b = sym_pos;
+    
+    o=1;
+    for i= 1:size(commonvox_pos,1)
+
+        if any(b(:,1)==commonvox_pos(i,1) & b(:,2)==commonvox_pos(i,2) & b(:,3)==commonvox_pos(i,3))
+
+            ind(o,:) = commonvox_pos(i,:);
+            o=o+1;
+        end
+    end 
+    
+    clear commonvox
+    commonvox_pos = ind;
+    clear ind
+end
+
+for id = 1:numel(patientID)
+    
+    clear mni_pos sym_pos
+    mni_pos = round(fp_getMNIpos(patientID{id}));  
+    [sym_pos, ~] = fp_symmetric_vol(mni_pos);
+    
+    for i = 1:size(commonvox_pos,1)
+        voxID{id}(i) = find(sym_pos(:,1)==commonvox_pos(i,1) & sym_pos(:,2)==commonvox_pos(i,2) & sym_pos(:,3)==commonvox_pos(i,3));
+    end
+    
+    
+end 
+
+
+
