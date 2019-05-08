dd10e782e0b3ae8e062ee635525b8639e58a1d32
“[franziska <franziskapellegrini@web.de>
2019-05-08 15:08:16 +0200

    tried to implement power estimate, but doesnt work uet

--- fp_tsdata_to_cpsd.m
+++ fp_tsdata_to_cpsd.m
@@ -3,11 +3,12 @@ function S = fp_tsdata_to_cpsd(X,fres,method,ind_1, ind_2, id_trials_1, id_trial
 % Estimate cross-power spectral density from time series data between
 % channels ind_1 and channels ind_2.
 % Shuffle id_lfp_trials for surrogate images. 
-
+keyboard
 [n_chan,n_times,n_trials] = size(X);
 X = demean(X);
 X = permute(X,[2 1 3]); % transpose row, col (works for single-trial data too)
 nfft = 2*fres;
+ind_pow = intersect(ind_1, ind_2);
 
 if ~exist('window','var')
     window = min(n_times,nfft); % according to Chronux ... by default Matlab 'pwelch' splits data into 8 overlapping segments
@@ -40,10 +41,28 @@ if strcmpi(method,'MT')
     
     S = 0;
     for r = 1:n_trials % works for single-trial too
-        %trial shuffling
+        
+        %trial shuffling           
         x_original = X(:,:,id_trials_1(r));
         x_perm = X(:,:,id_trials_2(r));
-        S = S + fp_cpsd_mt(x_original,x_perm,ind_1, ind_2,fres+1,window,noverlap,nchunks,taparray);
+        
+        clear s
+        s = fp_cpsd_mt(x_original,x_perm,ind_1, ind_2,fres+1,window,noverlap,nchunks,taparray);
+        
+        %replace power values by same-trial power values to keep them
+        %constant 
+        if ~isempty(ind_pow)
+            clear pow
+            pow = pmtm(x_original(:,ind_pow),nw,nfft,2*nfft);
+            for ii = 1:numel(ind_pow)
+                clear a b 
+                a = find(ind_1 == ind_pow(ii));
+                b = find(ind_2 == ind_pow(ii));
+                s(:,a,b) = pow(:,ii);
+            end
+        end
+        S = S+s;
+        
     end
     S = permute(S,[2 3 1])/n_trials;
     
