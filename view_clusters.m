e4f7def2b0b85b0d638e0c8851eed7414f944d14
“[franziska <franziskapellegrini@web.de>
2019-05-13 17:41:00 +0200

    minor changes

--- view_clusters.m
+++ view_clusters.m
@@ -1,7 +1,7 @@
-%% group 
+%% group
 
 clear all
-load('p_cluster_g_freq_abs.mat')
+load('p_e_cluster_g_freq_abs.mat')
 
 for iclus = 1:numel(p)
     if p(iclus) < 0.01
@@ -9,12 +9,12 @@ for iclus = 1:numel(p)
         a = true_clu;
         a(a~=iclus) = 0;
         
-        %spatial localization 
+        %spatial localization
         b = sum(a,1);
-        outname = sprintf('cluster_g_freq_abs_%d.nii',iclus);
+        outname = sprintf('cluster_e_g_freq_abs_%d.nii',iclus);
         fp_data2nii(b,nan,[],outname)
         
-        %spectral localization 
+        %spectral localization
         c = sum(a,2);
         figure
         bar(c)
@@ -23,18 +23,18 @@ for iclus = 1:numel(p)
         xticks = linspace(1,length(c), numel(xticklabels));
         set(gca,'XTick', xticks,'XTickLabel',xticklabels)
         
-        outname1 = sprintf('cluster_g_freq_abs_%d.png',iclus);
+        outname1 = sprintf('cluster_e_g_freq_abs_%d.png',iclus);
         print(outname1,'-dpng');
         close all
-
+        
     end
-   
+    
 end
 
 %% singlesub
 
 clear all
-load('p_cluster_ss_c_freq_abs.mat')
+load('p_e_cluster_ss_freq_abs.mat')
 patientID = {'04'; '07'; '08'; '09'; '10';'11';'12';'18';'20';'22';'25'}; %'12' has too few sensors
 
 
@@ -44,33 +44,37 @@ for isub = 1:numel(p)
     p1 = p{isub};
     true_clu = TRUE_CLU{isub};
     
-    for iclus = 1:numel(p1)
-        if p1(iclus) < 0.05
-            clear a b c
-            a = true_clu;
-            a(a~=iclus) = 0;
-
-            %spatial localization 
-            b = sum(a,1);
-            outname = sprintf('cluster_ss_c_freq_abs_%d_Patient%s.nii',iclus,patientID{isub});
-            fp_data2nii(b,nan,[],outname)
-
-            %spectral localization 
-            c = sum(a,2);
-            figure
-            bar(c)
-            xlabel('freqs')
-            xticklabels = 0:5:92;
-            xticks = linspace(1,length(c), numel(xticklabels));
-            set(gca,'XTick', xticks,'XTickLabel',xticklabels)
-
-            outname1 = sprintf('cluster_ss_c_freq_abs_%dPatient%s.png',iclus,patientID{isub});
-            print(outname1,'-dpng');
-            close all
-
+    if ~isempty(true_clu)
+        for iclus = 1:numel(p1)
+            if p1(iclus) < 0.05
+                clear a b c
+                a = true_clu;
+                a(a~=iclus) = 0;
+                
+                %spatial localization
+                b = sum(a,1);
+                
+                outname = sprintf('cluster_e_ss_freq_abs_%d_Patient%s.nii',iclus,patientID{isub});
+                fp_data2nii(b,nan,[],outname)
+                
+                %spectral localization
+                c = sum(a,2);
+                figure
+                bar(c)
+                xlabel('freqs')
+                xticklabels = 0:5:92;
+                xticks = linspace(1,length(c), numel(xticklabels));
+                set(gca,'XTick', xticks,'XTickLabel',xticklabels)
+                
+                outname1 = sprintf('cluster_e_ss_freq_abs_%dPatient%s.png',iclus,patientID{isub});
+                print(outname1,'-dpng');
+                close all
+                
+            end
         end
     end
     
+    
 end
 
-    
+
