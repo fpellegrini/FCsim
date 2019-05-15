80b76962fe2d55649baea029d8db3e664f18cadc
“[franziska <franziskapellegrini@web.de>
2019-05-15 16:00:13 +0200

    adjusted power issue

--- fp_cpsd_mt.m
+++ fp_cpsd_mt.m
@@ -1,10 +1,13 @@
 function S = fp_cpsd_mt(X1,X2,ind_1,ind_2,h,window,noverlap,nchunks,taparray)
 
+ind_pow = intersect(ind_1, ind_2);
 nfft = 2*(h-1);
 
 n1 = numel(ind_1);
 n2 = numel(ind_2);
+n3 = numel(ind_pow);
 S = complex(zeros(h,n1,n2));
+pow = zeros(h,n3);
 
 winstep = window-noverlap;
 ntapers = size(taparray,2);
@@ -26,15 +29,28 @@ for k = 1:nchunks
     
     for ii = 1:n1
         o = ind_1(ii);        
-        S(:,ii,:) = S(:,ii,:) + mean(P1(:,:,o) .* conj(P2(:,:,ind_2)),2);
+        S(:,ii,:) = S(:,ii,:) + mean(P1(:,:,o) .* conj(P2(:,:,ind_2)),2);        
     end
     
+    if ~isempty(ind_pow)
+        for pp = 1:n3 
+            u = ind_pow(pp);
+            pow(:,pp) = pow(:,pp) + mean(P1(:,:,u) .* conj(P1(:,:,u)),2);
+        end
+    end
+   
 end
 
 S = S/nchunks;
 
+if ~isempty(ind_pow)
+    pow = pow/nchunks;
 
-
-
-
+    for pp = 1:n3
+        clear a b 
+        a = find(ind_1 == ind_pow(pp));
+        b = find(ind_2 == ind_pow(pp));
+        S(:,a,b) = pow(:,pp);
+    end
+end
 
--- fp_tsdata_to_cpsd.m
+++ fp_tsdata_to_cpsd.m
@@ -3,12 +3,11 @@ function S = fp_tsdata_to_cpsd(X,fres,method,ind_1, ind_2, id_trials_1, id_trial
 % Estimate cross-power spectral density from time series data between
 % channels ind_1 and channels ind_2.
 % Shuffle id_lfp_trials for surrogate images. 
-keyboard
+%keyboard
 [n_chan,n_times,n_trials] = size(X);
 X = demean(X);
 X = permute(X,[2 1 3]); % transpose row, col (works for single-trial data too)
 nfft = 2*fres;
-ind_pow = intersect(ind_1, ind_2);
 
 if ~exist('window','var')
     window = min(n_times,nfft); % according to Chronux ... by default Matlab 'pwelch' splits data into 8 overlapping segments
@@ -24,7 +23,7 @@ if nargin < 3 || isempty(method)
    method = 'MT';    % default is multi-taper
 end
 
-if ~isequal(ind_1,unique(ind_1)) || ~isequal(ind_2, unique(ind_2))
+if ~isequal(sort(ind_1),sort(unique(ind_1))) || ~isequal(sort(ind_2), sort(unique(ind_2)))
     error('ind_1 and ind_2 must be unique')
 end
 
@@ -51,20 +50,7 @@ if strcmpi(method,'MT')
         x_perm = X(:,:,id_trials_2(r));
         
         clear s
-        s = fp_cpsd_mt(x_original,x_perm,ind_1, ind_2,fres+1,window,noverlap,nchunks,taparray);
-        
-        %replace power values by same-trial power values to keep them
-        %constant 
-        if ~isempty(ind_pow)
-            clear pow
-            pow = pmtm(x_original(:,ind_pow),nw,nfft,2*nfft);
-            for ii = 1:numel(ind_pow)
-                clear a b 
-                a = find(ind_1 == ind_pow(ii));
-                b = find(ind_2 == ind_pow(ii));
-                s(:,a,b) = pow(:,ii);
-            end
-        end
+        s = fp_cpsd_mt(x_original,x_perm,ind_1, ind_2,fres+1,window,noverlap,nchunks,taparray);       
         S = S+s;
         
     end
@@ -82,19 +68,6 @@ elseif strcmpi(method,'WELCH')
         
         clear XX
         s = fp_cpsd_welch(x_original,x_perm,ind_1,ind_2,fres+1,window,noverlap);
-        
-
-        if ~isempty(ind_pow)
-            clear pow
-            pow = pwelch(x_original(:,ind_pow),window,noverlap,nfft);
-            for ii = 1:numel(ind_pow)
-                clear a b 
-                a = find(ind_1 == ind_pow(ii));
-                b = find(ind_2 == ind_pow(ii));
-                s(a,b,:) = pow(:,ii);
-            end
-        end
-        
         S = S + s; 
     end
     S = S/n_trials; 
