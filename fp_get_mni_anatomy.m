b17f41516ca40c5bf95398bad7495db0b8d2937e
“[franziska <franziskapellegrini@web.de>
2019-06-27 14:57:03 +0200

    added cd back to data folder

--- fp_get_mni_anatomy.m
+++ fp_get_mni_anatomy.m
@@ -1,5 +1,5 @@
 function [label,code,id]=fp_get_mni_anatomy(coord)
-% keyboard
+
 cd ~/Dropbox/Master/Masterarbeit/MasterThesis/wjn_toolbox/spm12/toolbox/aal
 load('ROI_MNI_V5_List.mat')
 t1=wjn_read_nii('ROI_MNI_V5.nii');
@@ -19,3 +19,5 @@ else
         end
     end      
 end
+
+cd ~/Dropbox/Data_MEG_Project/
