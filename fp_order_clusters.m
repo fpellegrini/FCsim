eb6387dc26808815910f42909c7563ad9be07908
“[franziska <franziskapellegrini@web.de>
2019-08-06 10:46:21 +0200

    first commit

---
+++ fp_order_clusters.m
@@ -0,0 +1,10 @@
+function clu_sorted = fp_order_clusters(clu,total) 
+
+x = hist(clu(:),0:true_total);
+[~, order] = sort(x(2:end),'descend');
+
+clu_sorted = zeros(size(clu)); 
+
+for iclu = 1:total
+    clu_sorted(clu==order(iclu))=iclu; 
+end
