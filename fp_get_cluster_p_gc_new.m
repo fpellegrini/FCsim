7546c2a5013bddc95d03abaa3147e745885e6684
fpellegrini <franziskapellegrini@web.de>
2020-02-26 17:08:43 +0100

    bug fix

--- fp_get_cluster_p_gc_new.m
+++ fp_get_cluster_p_gc_new.m
@@ -2,7 +2,7 @@ function p = fp_get_cluster_p_gc_new(true_total, shuf_total, true_val, shuf_val,
 %fwf = testing method (less conservative): test first cluster against
 %first, second with second etc. Otherwise clusters are always compared
 %against the largest shuffled cluster. 
-
+% keyboard
 if (~exist('fwf','var'))|(fwf~=1)
     fwf = 0;
 end 
@@ -13,13 +13,13 @@ if true_total>0 %when at least one true cluster exists
         
         clear a shuf_clu_val
         clear b1 b2
-        b1 = shuf_clu(:,:,:,:,1);
-        b2 = shuf_clu(:,:,:,:,2);
+        b1 = shuf_clu(:,:,:,1);
+        b2 = shuf_clu(:,:,:,2);
         
         if fwf == 1
             shuf_clu_val = max(squeeze(sum(sum(shuf_val.*(b1==iclus),2),3))', squeeze(sum(sum(shuf_val.*(b2==iclus),2),3))');
         else
-            shuf_clu_val = max(squeeze(sum(sum(shuf_val.*(b1==1),2),3))', squeeze(sum(sum(shuf_val.*(b2==1),2),3))');
+            shuf_clu_val = max(abs(squeeze(sum(sum(shuf_val.*(b1==1),2),3)))', squeeze(abs(sum(sum(shuf_val.*(b2==1),2),3)))');
         end
         
         true_clu_val = sum(sum(sum(true_val(true_clu==iclus)))); %scalar
