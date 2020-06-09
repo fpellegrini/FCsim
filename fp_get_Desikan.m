d9ea8c69e764926467ca63b9cfe4525b3a400c77
fpellegrini <franziskapellegrini@web.de>
2020-06-09 14:38:40 +0200

    option to select more than 1 voxel per region

--- fp_get_Desikan.m
+++ fp_get_Desikan.m
@@ -1,4 +1,4 @@
-function D = fp_get_Desikan
+function D = fp_get_Desikan(iReg)
 
 load('./processed_bs/bs_results.mat')
 % number of ROIs in the Desikan-Kiliany Atlas
@@ -11,13 +11,14 @@ sub_ind_roi = {};
 for iROI = 1:nroi
     ind_roi{iROI} = cortex.Atlas(3).Scouts(iROI).Vertices;
     ind_cortex = cat(1, ind_cortex, ind_roi{iROI});
-    
+
     %index of roi voxels in ind_cortex
     [~, ind_roi_cortex{iROI}, ~] = intersect(ind_cortex, ind_roi{iROI}); 
     sub_ind_roi{iROI} = cortex.Atlas(3).Scouts(iROI).Vertices(...
-        randi(numel(cortex.Atlas(3).Scouts(iROI).Vertices)));
+        randperm(numel(cortex.Atlas(3).Scouts(iROI).Vertices),iReg));
+    
     sub_ind_cortex = cat(1,sub_ind_cortex, sub_ind_roi{iROI});
-    [~,sub_ind_roi_cortex{iROI},~] =  intersect(sub_ind_cortex, sub_ind_roi{iROI});%only one voxel per region
+%    [~,sub_ind_roi_cortex{iROI},~] =  intersect(sub_ind_cortex, sub_ind_roi{iROI});%only one voxel per region
     
 end
 
@@ -34,5 +35,6 @@ D.nvox = nvox;
 D.ind_cortex = ind_cortex;
 D.ind_roi_cortex = ind_roi_cortex;
 D.sub_ind_cortex = sub_ind_cortex;
+D.sub_ind_roi = sub_ind_roi;
 D.roi2vox = roi2vox;
 D.leadfield = leadfield;
