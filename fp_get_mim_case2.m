6efbb74c8539bdfc77ddb97e59492916a46128b0
fpellegrini <franziskapellegrini@web.de>
2020-06-10 18:13:24 +0200

    Now full frequency range again

--- fp_get_mim_case2.m
+++ fp_get_mim_case2.m
@@ -5,8 +5,8 @@ function [mic, mim] = fp_get_mim_case2(A,CS,fqA,D)
 A2 = reshape(A,nmeg,ni*nvox,nfreq);
 
 tic
-CSroi = zeros(nvox*ni,nvox*ni,nfreq/4);
-for ifq = 1: nfreq/4
+CSroi = zeros(nvox*ni,nvox*ni,nfreq);
+for ifq = 1: nfreq
     CSroi(:,:,ifq) = squeeze(A2(:,:,fqA(ifq)))' * CS(:,:,ifq)...
         * squeeze(A2(:,:,fqA(ifq)));
 end
@@ -15,7 +15,7 @@ toc
 %divide by power to obtain coherence
 tic
 Cohroi = zeros(size(CSroi));
-for ifreq = 1: nfreq/4
+for ifreq = 1: nfreq
     clear pow
     pow = real(diag(CSroi(:,:,ifreq)));
     Cohroi(:,:,ifreq) = CSroi(:,:,ifreq)./ sqrt(pow*pow');
