dc5221b7ebb1d813d180f40053b70bdd84314b97
fpellegrini <franziskapellegrini@web.de>
2020-06-17 15:08:23 +0200

    minor changes

--- fp_cluster_mim.m
+++ fp_cluster_mim.m
@@ -1,24 +1,22 @@
-efunction fp_cluster_mim(DIROUT, abs_imag,testmethod,alpha,fwf,j)
+function fp_cluster_mim(DIROUT,testmethod,alpha,fwf,j)
 
 %Group statistics on megmeg data.
 %Clustering group statistics across space and frequencies.
 %
 %Input:
-%
-%abs_imag = either 'abs' or 'imag'
+%DIROUT
 %
 %testmethod = either 's' for signrank testing or 't' for simple
 %thresholding
 %
 %fwf: testing method (less conservative): test first cluster against
-%
 %first, second with second etc. By default, clusters are always compared
 %against the largest shuffled cluster.
 %
 %j=1 : only Julian's subjects
 
 
-% fp_addpath_sabzi
+fp_addpath
 
 if ~exist(DIROUT); mkdir(DIROUT); end
 
@@ -36,27 +34,22 @@ end
 if j == 0
     j_s = 'allsubs';
     patientID = {'04'; '07'; '08'; '09'; '10';'11';'12';'18';'20';'22';'25'};
-    [~, voxID] = fp_find_commonvox;
 elseif j == 1
     j_s = 'j';
     patientID = {'04'; '07'; '09'; '10';'11';'20';'22';'25'};
-    [~, voxID] = fp_find_commonvox;
-    voxID([3,7,8])=[];
 else
     error('Wrong input j')
 end
 
-minnbchan = 2;
-
 %% load data
 
 for id = 1:numel(patientID)
-    load(sprintf('./roi_MIM_sub%s.mat',patientID{id}))
+    load(sprintf('./roi_MIM90_sub%s.mat',patientID{id}))
     
-    mim_true(id,:,:,:) = MIM_TRUE(id,:,:,:); 
-    mim_shuf(id,:,:,:,:) = MIM_SHUF(:,id,:,:,:);
-    tCoh(id,:,:,:) = MIC_TRUE(id,:,:,:); 
-    sCoh(id,:,:,:,:) = MIC_SHUF(:,id,:,:,:);
+    mim_true(id,:,:,:) = MIM_TRUE; 
+    mim_shuf(id,:,:,:,:) = MIM_SHUF;
+    tCoh(id,:,:,:) = MIC_TRUE; 
+    sCoh(id,:,:,:,:) = MIC_SHUF;
     
     clear MIM_TRUE MIC_TRUE MIM_SHUF MIC_SHUF
 end
