bc66980cbe37597d8b25620ed66174c3dc5af7e7
fpellegrini <franziskapellegrini@web.de>
2020-06-22 12:21:49 +0200

    included mim measure

--- fp_cluster_mim.m
+++ fp_cluster_mim.m
@@ -48,8 +48,8 @@ for id = 1:numel(patientID)
     
     mim_true(id,:,:,:) = MIM_TRUE; 
     mim_shuf(id,:,:,:,:) = MIM_SHUF;
-    tCoh(id,:,:,:) = MIC_TRUE; 
-    sCoh(id,:,:,:,:) = MIC_SHUF;
+    mic_true(id,:,:,:) = MIC_TRUE; 
+    mic_shuf(id,:,:,:,:) = MIC_SHUF;
     
     clear MIM_TRUE MIC_TRUE MIM_SHUF MIC_SHUF
 end
@@ -60,14 +60,15 @@ end
 fprintf('Testing...\n')
 if strcmp(testmethod,'s')
     tic
-    [true_p,onoff,true_val] = fp_get_signrank_results_megmeg(tCoh,sCoh,alpha);
+    [true_p_mic,onoff_mic,true_val_mic] = fp_get_signrank_results_megmeg(mic_true,mic_shuf,alpha);
+    [true_p_mim,onoff_mim,true_val_mim] = fp_get_signrank_results_megmeg(mim_true,mim_shuf,alpha);
     toc
     
 elseif strcmp(testmethod,'t')
-    threshold = prctile(reshape(sum(sCoh,1),1,[]),99.9);
-    true_val = squeeze(sum(tCoh,1));
-    onoff = true_val > threshold;
-    true_p = [];
+    threshold = prctile(reshape(sum(mic_shuf,1),1,[]),99.9);
+    true_val_mic = squeeze(sum(mic_true,1));
+    onoff_mic = true_val_mic > threshold;
+    true_p_mic = [];
     
 else
     error('Testmethod unknown.')
@@ -76,7 +77,8 @@ end
 
 fprintf('Finding clusters...\n')
 kron_conn = fp_get_kron_conn_megmeg(nfreq);
-[true_clu, true_total] = fp_get_cluster_components_megmeg(onoff,kron_conn);
+[true_clu_mic, true_total_mic] = fp_get_cluster_components_megmeg(onoff_mic,kron_conn);
+[true_clu_mim, true_total_mim] = fp_get_cluster_components_megmeg(onoff_mim,kron_conn);
 %findclusters is not appropriate for this application
 
 %% shuffled
@@ -87,39 +89,48 @@ for iit = 1:nit
     clear onoff cCoh csCoh
     
     %select current shuffled coh
-    cCoh = squeeze(sCoh(:,iit,:,:,:));
+    cCoh_mic = squeeze(mic_shuf(:,iit,:,:,:));
+    cCoh_mim = squeeze(mim_shuf(:,iit,:,:,:));
     
     fprintf('Testing...\n')
     tic
     if strcmp(testmethod,'s')
         
         if iit == 1
-            csCoh = sCoh(:,2:end,:,:,:);
+            csCoh_mic = mic_shuf(:,2:end,:,:,:);
+            csCoh_mim = mim_shuf(:,2:end,:,:,:);
         elseif iit == nit
-            csCoh = sCoh(:,1:end-1,:,:,:);
+            csCoh_mic = mic_shuf(:,1:end-1,:,:,:);
+            csCoh_mim = mim_shuf(:,1:end-1,:,:,:);
         else
-            csCoh = sCoh(:,[1:iit-1 iit+1:end],:,:,:);
+            csCoh_mic = mic_shuf(:,[1:iit-1 iit+1:end],:,:,:);
+            csCoh_mim = mim_shuf(:,[1:iit-1 iit+1:end],:,:,:);
         end
         
-        [~,onoff,shuf_val(iit,:,:,:)] = fp_get_signrank_results_megmeg(cCoh,csCoh,alpha);
+        [~,onoff_mic,shuf_val_mic(iit,:,:,:)] = fp_get_signrank_results_megmeg(cCoh_mic,csCoh_mic,alpha);
+        [~,onoff_mim,shuf_val_mim(iit,:,:,:)] = fp_get_signrank_results_megmeg(cCoh_mim,csCoh_mim,alpha);
         
     elseif strcmp(testmethod,'t')
-        onoff = squeeze(sum(cCoh,1)) > threshold;
-        shuf_val(iit,:,:,:) = squeeze(sum(cCoh,1));
+        onoff_mic = squeeze(sum(cCoh_mic,1)) > threshold;
+        shuf_val_mic(iit,:,:,:) = squeeze(sum(cCoh_mic,1));
     end
     toc
     
     fprintf('Finding clusters...\n')
-    [shuf_clu(iit,:,:,:), shuf_total(iit)] = fp_get_cluster_components_megmeg(onoff,kron_conn);
+    [shuf_clu_mic(iit,:,:,:), shuf_total_mic(iit)] = fp_get_cluster_components_megmeg(onoff_mic,kron_conn);
+    [shuf_clu_mim(iit,:,:,:), shuf_total_mim(iit)] = fp_get_cluster_components_megmeg(onoff_mim,kron_conn);
         
     
 end
 
 
 %%
-p = fp_get_cluster_p_megmeg(true_total, shuf_total, true_val, shuf_val, true_clu, shuf_clu, fwf);
+p_mic = fp_get_cluster_p_megmeg(true_total_mic, shuf_total_mic, true_val_mic, shuf_val_mic, true_clu_mic, shuf_clu_mic, fwf);
+p_mim = fp_get_cluster_p_megmeg(true_total_mim, shuf_total_mim, true_val_mim, shuf_val_mim, true_clu_mim, shuf_clu_mim, fwf);
 
 %%
 outname = sprintf('./mim90_pval');
 
-save(outname,'p','true_total','true_clu','true_p','true_val','-v7.3')
+save(outname,'p_mic','true_total_mic','true_clu_mic','true_p_mic',...
+    'true_val_mic','p_mim','true_total_mim','true_clu_mim','true_p_mim',...
+    'true_val_mim','-v7.3')
