12bed12a3e4b8a25c53875c3b28643ca6e1a5b54
fpellegrini <franziskapellegrini@web.de>
2020-11-30 12:01:50 +0100

    Region-wise analysis

--- fp_TWSTRS_lfpmeg.m
+++ fp_TWSTRS_lfpmeg.m
@@ -8,7 +8,7 @@ scores = db_twstrs();
 alpha = 0.05;
 
 nsubs = numel(patientID);
-%%
+%% collect twstrs scores, MIC and GC of all subjects 
 o = 1;
 for id = 1:nsubs
     clearvars -except DIRDATA patientID scores alpha nsubs o id t_score tCoh granger
@@ -60,7 +60,8 @@ end
 [p_c_corrected, mask_c] = fdr(pval_c,0.05);
 [p_g_corrected, mask_g] = fdr(pval_g,0.05);
 
-%%
+%% sum across all freqs
+
 figure
 imagesc(sum(mask_c,3))
 xlabel('Regions')
@@ -84,3 +85,37 @@ set(gca,'XTick', xticks,'XTickLabel',xticklabels,'YTick',xticks,'YTickLabel',xti
 xtickangle(45)
 colorbar
 title('GC') 
+
+%% sum of significant freq bins per roi-roi-bin, divided by the number of freq bins in this band.
+% So the colorbar is fixed from 0 to 1.
+
+fbands = [1 4; 4 7; 7 15; 15 30; 30 45]; %theta, alpha, beta, low gamma, high gamma
+fnames = {'theta', 'alpha', 'beta', 'low gamma', 'high gamma'};
+
+figone(30,60)
+for ibands = 1:size(fbands,1)
+    subplot(2,5,ibands)
+    imagesc(sum(mask_c(:,:,fbands(ibands,1):fbands(ibands,2)),3)./length(fbands(ibands,1):fbands(ibands,2)))
+    title(['Corr Coh-TWSTRS ', fnames{ibands}])
+    xlabel('Regions')
+    ylabel('Regions')
+    xticks=1:10;
+    xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+        'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+    set(gca,'XTick', xticks,'XTickLabel',xticklabels)
+    xtickangle(45)
+    caxis([0 1])
+    
+    subplot(2,5,ibands+5)
+    imagesc(sum(mask_g(:,:,fbands(ibands,1):fbands(ibands,2)),3)./length(fbands(ibands,1):fbands(ibands,2)))
+    title(['Corr GC-TWSTRS ' , fnames{ibands}])
+    xlabel('Regions')
+    ylabel('Regions')
+    xticks=1:10;
+    xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+        'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+    set(gca,'XTick', xticks,'XTickLabel',xticklabels)
+    xtickangle(45)
+    caxis([0 1])
+end
+
--- fp_lfpmegmeg_ana.m
+++ fp_lfpmegmeg_ana.m
@@ -8,12 +8,12 @@ patientID = {'04'; '07'; '08'; '09'; '10';'11';'12';'18';'20';'22';'25'};
 
 for id = 1: numel(patientID)
     
-    %% imCoh 
+    %% imCoh (mim/mic) 
     clearvars -except DIRIN patientID id nit DIRFIG mim mic mic_shuf mim_shuf
     load(['MIM_GC_sub' patientID{id} '.mat'])
     nit = size(MIC_SHUF,1);
-        
     
+    % p values of permutation testing per sub, roi-roi combi and freq 
     for ii = 1:size(MIC_TRUE,1)
         for jj = 1: size(MIC_TRUE,2)
             for ifq = 1: size(MIC_TRUE,3)
@@ -25,10 +25,13 @@ for id = 1: numel(patientID)
         end
     end
     
+    %collect mim and mic values of every sub 
     mic(id,:,:,:) = MIC_TRUE;
     mim(id,:,:,:) = MIM_TRUE; 
     mim_shuf(id,:,:,:,:) = MIM_SHUF(1:100,:,:,:); 
     mic_shuf(id,:,:,:,:) = MIC_SHUF(1:100,:,:,:); 
+    
+    %% GC
     
     % indices of DIFFGC_TRUE
     ninds=1;
@@ -46,7 +49,7 @@ for id = 1: numel(patientID)
     
     granger(id,:,:,:) = granger1; 
     
-    %%
+    %% subject-wise plots of true mim/mic
 %     figone(40,40)
 %     subplot(2,2,1)
 %     imagesc(squeeze(sum(MIC_TRUE,3)))
@@ -113,7 +116,7 @@ for id = 1: numel(patientID)
 %     saveas(gcf,outname, 'png')
 %     close all
     
-    %% GC
+    %% subject-wise plots of true GC
 %     roi = 7;
 %     
 %     figone(15,40) 
@@ -153,10 +156,10 @@ for id = 1: numel(patientID)
 %     
     
 end 
-%%
+
 [nsubs,nroi,~,nfreq] = size(granger);
 
-%% true
+%% between subjects for 5 freq bands 
 fprintf('Testing...\n')
 
 [p_gc,~,~,~] = fp_get_signrank_results_gc(granger,0.001);
@@ -170,19 +173,105 @@ sum(mask1(:))
 [p_mic,~,~] = fp_get_signrank_results_megmeg(mic,mic_shuf,0.001);
 [p2, mask2] = fdr(p_mic, 0.05);
 sum(mask2(:))
-%%
+%
 fbands = [1 4; 4 7; 7 15; 15 30; 30 45]; %theta, alpha, beta, low gamma, high gamma
+fnames = {'theta', 'alpha', 'beta', 'low gamma', 'high gamma'};
 
+figone(30,60)
 for ibands = 1: size(fbands,1)
-    figure
-    subplot(1,2,1)
-    imagesc(squeeze(sum(mask1(:,:,fbands(ibands,1):fbands(ibands,2)),3)))
-    title('MIM')
     
-    subplot(1,2,2)
-    imagesc(squeeze(sum(mask2(:,:,fbands(ibands,1):fbands(ibands,2)),3)))
-    title('MIC')
+    subplot(2,5,ibands)
+    imagesc(squeeze(sum(mask1(:,:,fbands(ibands,1):fbands(ibands,2)),3))./length(fbands(ibands,1):fbands(ibands,2)))
+    title(['MIM ', fnames{ibands}])
+    xlabel('Regions')
+    ylabel('Regions')  
+    xticks=1:10;
+    xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+        'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+    set(gca,'XTick', xticks,'XTickLabel',xticklabels)
+    xtickangle(45)
+    caxis([0 1])
+    
+    subplot(2,5,ibands+5)
+    imagesc(squeeze(sum(mask2(:,:,fbands(ibands,1):fbands(ibands,2)),3))./length(fbands(ibands,1):fbands(ibands,2)))
+    title(['MIC ', fnames{ibands}])
+    xlabel('Regions')
+    ylabel('Regions')  
+    xticks=1:10;
+    xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+        'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+    set(gca,'XTick', xticks,'XTickLabel',xticklabels)
+    xtickangle(45)
+    caxis([0 1])
 end
 
-%%
+%% within subjects for 5 freq bands 
 
+for iband = 1: size(fbands,1)   
+    mim_bands(:,:,:,iband) = squeeze(sum(mim(:,:,:,fbands(ibands,1):fbands(ibands,2)),4));
+    mic_bands(:,:,:,iband) = squeeze(sum(mic(:,:,:,fbands(ibands,1):fbands(ibands,2)),4));
+    
+    mim_shuf_bands(:,:,:,:,ibands) = squeeze(sum(mim_shuf(:,:,:,:,fbands(ibands,1):fbands(ibands,2)),5));
+    mic_shuf_bands(:,:,:,:,ibands) = squeeze(sum(mic_shuf(:,:,:,:,fbands(ibands,1):fbands(ibands,2)),5));
+end
+
+for isub = 1: size(mim,1)
+    for iroi = 1:size(mim,2)
+        for jroi = 1:size(mim,3)
+            for iband = 1: size(fbands,1)
+                
+                p_mim_bands(isub,iroi,jroi,iband) = sum(mim_shuf_bands(isub, :,iroi,jroi,iband)>mim_bands(isub,iroi,jroi,iband))/nit;
+                p_mic_bands(isub,iroi,jroi,iband) = sum(mic_shuf_bands(isub, :,iroi,jroi,iband)>mic_bands(isub,iroi,jroi,iband))/nit;
+            end
+        end
+    end
+end
+
+logp_mim = -log10(p_mim_bands); 
+logp_mim(logp_mim<=1.3)=0;
+logp_mim(isinf(logp_mim))=2;
+logp_mic = -log10(p_mic_bands); 
+logp_mic(logp_mic<=1.3)=0;
+logp_mic(isinf(logp_mic))=2;
+
+
+for iband = 1:size(fbands,1)
+    figure 
+    figone(30,60)
+    for isub = 1:size(mim,1) 
+
+        subplot(3,4,isub) 
+        imagesc(squeeze(logp_mim(isub,:,:,iband)))
+        title(['MIM ', fnames{iband}])
+        xlabel('Regions')
+        ylabel('Regions')  
+        xticks=1:10;
+        xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+            'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+        set(gca,'XTick', xticks,'XTickLabel',xticklabels,'YTick',xticks,'YTickLabel',xticklabels)
+        xtickangle(45)
+        colorbar
+        caxis([0 2])
+        
+    end 
+end 
+
+for iband = 1:size(fbands,1)
+    figure 
+    figone(30,60)
+    for isub = 1:size(mim,1) 
+        
+        subplot(3,4,isub)
+        imagesc(squeeze(logp_mic(isub,:,:,iband)))
+        title(['MIC ', fnames{iband}])
+        xlabel('Regions')
+        ylabel('Regions')  
+        xticks=1:10;
+        xticklabels = {'Precentral left','Precentral right', 'SMA left', 'SMA right', 'Parietal left',...
+            'Parietal right', 'cerebellum','pallidum','LFP right','LFP left'};
+        set(gca,'XTick', xticks,'XTickLabel',xticklabels,'YTick',xticks,'YTickLabel',xticklabels)
+        xtickangle(45)
+        colorbar
+        caxis([0 2])
+    end 
+end 
