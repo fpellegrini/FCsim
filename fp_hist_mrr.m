269dffa8e704dfdf1fd08742790d7d116f459a30
fpellegrini <franziskapellegrini@web.de>
2021-02-10 12:17:25 +0100

    further refinements

--- fp_hist_mrr.m
+++ fp_hist_mrr.m
@@ -19,42 +19,42 @@ name = {...
     'ip5_iss1';...
     'ip6_lag1';...
     'ip7_dics';...
-    'ip7_eloreta';...
+    'ip7_eloreta_reg';...
     'ip8_hemisym1'};
 
 labs = {'MIC','MIM','Mean icoh','mean abscoh'};
 
 
 
-for iname = 1:numel(name)
+for iname = 1:5%numel(name)
     
     clearvars -except mdefault DIRDATA DIRFIG name iname labs
     load([DIRDATA 'mrr_mim2_' name{iname} '.mat']);
-      
-    %     count = 1;
-    for imim = 1:3
+    
+    for imim = 1:4
         
-        for imsr = 1:3
+        for imsr = 1:2
             if imsr==1
-                data = mrr(:,:,imim);
+                if iname == 1
+                    data = cat(2,mrr(:,12:end,imim),mrr(:,8:11,imim)); %zs0 + sumVox + baseline + 2 corrected pips
+                else
+                    data = mrr(:,:,imim);
+                end
             elseif imsr == 2
-                data = pr(:,:,imim);
+                if iname==1
+                    data = cat(2,pr(:,12:end,imim),pr(:,8:11,imim));
+                else
+                    data = pr(:,:,imim);
+                end
             end
             
             if (imim == 1 || imim ==2)
                 npips = size(data,2);
-                %                 npips1 = size(data,2);
+            elseif iname ==1 && imim>2
+                data(:,8:9,:)=[];%no baseline, no sumVox, but corrected % pipelines 
+                npips = size(data,2);
             else
-                npips = size(data,2)-1;
-                %             elseif inames==1 && imim ==3
-                %                 npips = size(data,2)-2;
-                %                 npips1 = size(data,2);
-                %             elseif inames>1 && imim ==3
-                %                 npips = size(data,2)-1;
-                %                 npips1 = size(data,2);
-                %             else
-                %                 npips = size(data,2);
-                %                 npips1 = size(data,2);
+                npips = size(data,2)-1; %no baseline
             end
             
             figure
@@ -63,23 +63,25 @@ for iname = 1:numel(name)
             for ipip = 1:npips
                 
                 data1 = squeeze(data(:,ipip));
-                xbins =linspace(0.05,0.95,10); %linspace(0,1,10);
+                xbins =linspace(0.05,0.95,10);
                 [counts,bins] = hist(data1,xbins);
-                %                 subplot(2,npips1,ipip+(count*npips1)-npips1)
                 subplot(1,npips,ipip)
                 barh(bins,counts)
                 grid on
                 
-                xTicks = (1:npips).*3;
                 if iname == 1
-                    xticklabels = {'1PC','2PCs', '3PCs','4PCs','5PCs','99%', '90%','sumVox','baseline'};
-%                     xticklabels = {'1 pc','2 pc', '3 pc', '4 pc', '5 pc', '99%', '90%','baseline'};
-                    
+                    if imim <3
+                        xtitles = {'1PC','2PCs', '3PCs','4PCs','5PCs','99%',...
+                            '90%','sumVox','baseline','99% corrected','90% corrected'};
+                    else
+                        xtitles = {'1PC','2PCs', '3PCs','4PCs','5PCs','99%',...
+                            '90%','99% corrected','90% corrected'};
+                    end
                 else
-                    xticklabels = {'1 pc','2 pc', '3 pc', '4 pc', '5 pc', '99%', '90%','baseline'};
+                    xtitles = {'1PC','2PCs', '3PCs','4PCs','5PCs','99%', '90%','baseline'};
                 end
                 
-                title(xticklabels(ipip))
+                title(xtitles(ipip))
                 
                 if imsr==1
                     ylabel([labs{imim} ' MRR'])
@@ -91,7 +93,6 @@ for iname = 1:numel(name)
                 ylim([0 1])
                 
             end
-            %             count = count +1;
             
             if imsr ==1
                 outname = [DIRFIG name{iname} '_' labs{imim} '_mrr'];
@@ -101,8 +102,55 @@ for iname = 1:numel(name)
             saveas(gcf,outname, 'png')
             close all
             
+            
+            %% ZS pipeline
+            if iname ==1
+                if imsr==1
+                    data = mrr(:,1:7,imim); %zs pipelines 
+                elseif imsr == 2
+                    data = pr(:,1:7,imim);
+                end
+                npips = size(data,2);
+                
+                figure
+                figone(15,50)
+                
+                for ipip = 1:npips
+                    
+                    data1 = squeeze(data(:,ipip));
+                    xbins =linspace(0.05,0.95,10);
+                    [counts,bins] = hist(data1,xbins);
+                    subplot(1,npips,ipip)
+                    barh(bins,counts)
+                    grid on
+                    
+                    xtitles = {'1PC','2PCs', '3PCs','4PCs','5PCs','99%', '90%'};
+                    title(xtitles(ipip))
+                    
+                    if imsr==1
+                        ylabel([labs{imim} ' MRR'])
+                    else
+                        ylabel([labs{imim} ' PR'])
+                    end
+                    
+                    xlim([0 103])
+                    ylim([0 1])
+                    
+                end
+                
+                if imsr ==1
+                    outname = [DIRFIG 'ip9_ZS_' labs{imim} '_mrr'];
+                else
+                    outname = [DIRFIG 'ip9_ZS_' labs{imim} '_pr'];
+                end
+                
+                saveas(gcf,outname, 'png')
+                close all
+            end
+            %%
+            
         end
-              
+        
     end
     
 end
