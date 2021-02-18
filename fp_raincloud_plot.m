5e15a043995e19ae051036db55d6fd616559dcd9
fpellegrini <franziskapellegrini@web.de>
2021-02-18 12:55:21 +0100

    raincloud plots instead of histograms

---
+++ fp_raincloud_plot.m
@@ -0,0 +1,104 @@
+%% raincloud_plot - plots a combination of half-violin, boxplot,  and raw
+% datapoints (1d scatter).
+% Use as h = raincloud_plot(X, cl), where X is a data vector and cl is an
+% RGB value. h is a cell array of handles for the various figure parts.
+% Optional 3rd input argument 'density_type' can be 'ks' (default) or 'rash'.
+% 'ks' uses matlab's 'ksdensity'. 'rash' uses 'rst_RASH' from Cyril
+% Pernet's robust statistics toolbox (which must be on the matlab path).
+% Based on https://micahallen.org/2018/03/15/introducing-raincloud-plots/
+% Inspired by https://m.xkcd.com/1967/
+% Written by Tom Marshall. www.tomrmarshall.com
+% Thanks to Jacob Bellmund for some improvements
+
+
+function [h, u] = fp_raincloud_plot(X, cl, box_on, bandwidth, density_type)
+
+% if ~exist('density_type', 'var') | isempty(density_type)
+%     density_type = 'ks'; % default is 'ks', can also be 'rash'
+% end
+
+if nargin < 3
+    box_on = 1;
+end
+
+if nargin < 4
+    bandwidth = [];
+end
+
+if nargin < 5
+    density_type = 'ks';
+end
+
+% calculate kernel density
+switch density_type
+    case 'ks'
+        [f, Xi, u] = ksdensity(X, 'bandwidth', bandwidth);
+    case 'rash'
+        try
+            [Xi, f] = rst_RASH(X);
+            u = NaN; % not sure how to handle this with RASH yet
+        catch
+            disp('you''ve specified density_type = ''RASH'', but something''s gone wrong.')
+            disp('Have you downloaded Cyril Pernet''s robust stats toolbox?');
+        end
+end
+
+% density plot
+h{1} = area(Xi, f); hold on
+set(h{1}, 'FaceColor', cl);
+set(h{1}, 'EdgeColor', [1 1 1]);
+set(h{1}, 'LineWidth', 2);
+plot([0 1],[0 0],'color',[1 1 1],'LineWidth',2)
+
+% make some space under the density plot for the boxplot
+yl = get(gca, 'YLim');
+b = yl(2)/3;
+set(gca, 'YLim', [-b*2 yl(2)]);
+
+% width of boxplot
+% wdth = b*0.5;
+wdth = 0.5;
+
+% jitter for raindrops
+jit = (rand(size(X)) - 0.5) * wdth;
+
+% info for making boxplot
+quartiles   = quantile(X, [0.25 0.75 0.5]);
+iqr         = quartiles(2) - quartiles(1);
+if iqr ~= 0 
+    Xs          = sort(X);
+    whiskers(1) = min(Xs(Xs > (quartiles(1) - (1.5 * iqr))));
+    whiskers(2) = max(Xs(Xs < (quartiles(2) + (1.5 * iqr))));
+    Y           = [quartiles whiskers];
+else 
+    Y = quartiles;
+end
+
+% raindrops
+h{2} = scatter(X, jit - 0.5);
+%h{2} = scatter(X, jit - b/2);
+h{2}.SizeData = 10;
+h{2}.MarkerFaceColor = cl;
+h{2}.MarkerEdgeColor = 'none';
+
+if box_on
+    % 'box' of 'boxplot'
+    g = [0.3 0.3 0.3];
+    h{3} = rectangle('Position', [Y(1) b/10-(wdth*0.5) Y(2)-Y(1) wdth]);   
+    %h{3} = rectangle('Position', [Y(1) -b/2-(wdth*0.5) Y(2)-Y(1) wdth]);
+    set(h{3}, 'EdgeColor', g)
+    set(h{3}, 'LineWidth', 2);
+    % could also set 'FaceColor' here as Micah does, but I prefer without
+    
+    % mean line    
+    h{4} = line([Y(3) Y(3)], [b/10-(wdth*0.5) b/10+(wdth*0.5)], 'col', 'k', 'LineWidth', 3);
+    %h{4} = line([Y(3) Y(3)], [-b/2-(wdth*0.5) -b/2+(wdth*0.5)], 'col', 'r', 'LineWidth', 2);
+    
+    % whiskers
+    if iqr ~= 0 
+        h{5} = line([Y(2) Y(5)], [b/10 b/10], 'col',g, 'LineWidth', 2);
+        h{6} = line([Y(1) Y(4)], [b/10 b/10], 'col',g, 'LineWidth', 2);
+        %h{5} = line([Y(2) Y(5)], [-b/2 -b/2], 'col', 'k', 'LineWidth', 2);
+        %h{6} = line([Y(1) Y(4)], [-b/2 -b/2], 'col', 'k', 'LineWidth', 2);
+    end
+end
