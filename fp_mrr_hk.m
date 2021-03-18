9a71f3654189845e869a2a94cb55a034450d17ab
fpellegrini <franziskapellegrini@web.de>
2021-03-18 19:17:02 +0100

    added possibility for GC evaluation with shole pipeline

--- fp_mrr_hk.m
+++ fp_mrr_hk.m
@@ -1,4 +1,4 @@
-function [mrr, pr,hk] = fp_mrr_hk(cc,iroi_seed,iroi_tar)
+function [mrr, pr,hk] = fp_mrr_hk(cc,iroi_seed,iroi_tar,half_m)
 
 if norm(iroi_seed(:)- iroi_tar(:),'fro')==0
     mrr = 0;
@@ -8,12 +8,19 @@ else
 
     nints = numel(iroi_seed);
     nroi = size(cc,1);
-    inds = find(triu(ones(nroi),1));
+    
+    if half_m 
+        inds = find(triu(ones(nroi),1));
+    else
+        inds = find(ones(nroi));
+    end
 
     label= zeros(nroi,nroi);
     for iint = 1:numel(iroi_seed)
         label(iroi_seed(iint),iroi_tar(iint)) = 1;
-        label(iroi_tar(iint),iroi_seed(iint)) = 1;
+        if half_m %when only positive DIFFGC is submitted as cc
+            label(iroi_tar(iint),iroi_seed(iint)) = 1;
+        end
     end
     lab = label(:);
     tr = find(lab(inds)==1);
