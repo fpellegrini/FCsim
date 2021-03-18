8278b44974cee59dd3e16298442436102bd33a43
fpellegrini <franziskapellegrini@web.de>
2021-03-18 19:24:18 +0100

    Now it calls the newly structured pipeline

--- fp_eval_mim_struct_sim.m
+++ fp_eval_mim_struct_sim.m
@@ -14,7 +14,7 @@ iit = str2num(getenv('SGE_TASK_ID'))
 % for ip = varyParam
 clear nInteractions nRegionInts SNR noise_mix nlag filtertype hemisym
 
-[nInteractions,nRegionInts,SNR,noise_mix,nlag,filtertype,hemisym] = fp_get_params(ip);
+[nInteractions,nRegionInts,SNR,noise_mix,nlag,filtertype] = fp_get_params(ip);
 ip
 for iInt = nInteractions
     for iReg = nRegionInts
@@ -22,39 +22,36 @@ for iInt = nInteractions
             for iss = noise_mix
                 for ilag = nlag
                     for ifi = 1:numel(filtertype)
-                        for ihemi = hemisym
+                        
+                        ifilt = filtertype{ifi};
+                        
+                        iit
+                        %create logfile for parallization
+                        logname = sprintf('iInt%d_iReg%d_snr0%d_iss0%d_lag%d_filt%s_hemisym%d_iter%d'...
+                            ,iInt,iReg,isnr*10,iss*10, ilag,ifilt,ihemi,iit);
+                        %
+                        if ~exist(sprintf('%s%s_work',DIRLOG,logname)) & ~exist(sprintf('%s%s_done',DIRLOG,logname))
+                            eval(sprintf('!touch %s%s_work',DIRLOG,logname))
+                            fprintf('Working on %s. \n',logname)
                             
-                            ifilt = filtertype{ifi};
                             
-                            iit
-                            %create logfile for parallization
-                            logname = sprintf('iInt%d_iReg%d_snr0%d_iss0%d_lag%d_filt%s_hemisym%d_iter%d'...
-                                ,iInt,iReg,isnr*10,iss*10, ilag,ifilt,ihemi,iit);
-                            %
-                            if ~exist(sprintf('%s%s_work',DIRLOG,logname)) & ~exist(sprintf('%s%s_done',DIRLOG,logname))
-                                eval(sprintf('!touch %s%s_work',DIRLOG,logname))
-                                fprintf('Working on %s. \n',logname)
-                                
-                                
-                                params.iInt = iInt;
-                                params.iReg = iReg;
-                                params.isnr = isnr;
-                                params.iss = iss;
-                                params.ilag = ilag;
-                                params.ifilt = ifilt;
-                                params.ihemi = ihemi;
-                                params.iit = iit;
-                                params.ip = ip;
-                                params.logname = logname; 
-                                
-                                
-                                fp_mim_struct_sim(params)
-                                
-                                eval(sprintf('!mv %s%s_work %s%s_done',DIRLOG,logname,DIRLOG,logname))
-                                
-                            end  %work_done
+                            params.iInt = iInt;
+                            params.iReg = iReg;
+                            params.isnr = isnr;
+                            params.iss = iss;
+                            params.ilag = ilag;
+                            params.ifilt = ifilt;
+                            params.iit = iit;
+                            params.ip = ip;
+                            params.logname = logname;
                             
-                        end %hemispheres
+                            
+                            fp_data2mim_sim(params)
+                            
+                            eval(sprintf('!mv %s%s_work %s%s_done',DIRLOG,logname,DIRLOG,logname))
+                            
+                        end  %work_done
+                        
                     end %filtertype
                 end %lag
             end %noise_mix
