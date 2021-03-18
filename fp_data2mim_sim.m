f7684ecc2be1895bda93d3b127b1c77e032fc2ca
fpellegrini <franziskapellegrini@web.de>
2021-03-18 19:16:07 +0100

    Added pipelines 20 and 21 and implemented champ filter. Additional GC evaluation.

--- fp_data2mim_sim.m
+++ fp_data2mim_sim.m
@@ -6,6 +6,15 @@ if ~exist(DIROUT);mkdir(DIROUT); end
 DIROUT1 = '/home/bbci/data/haufe/Franziska/data/mim_save3/';
 if ~exist(DIROUT1);mkdir(DIROUT1); end
 
+%define which pipelines run with this configuration 
+if params.ip == 1
+    pips = 1:21;
+elseif strcmp(params.ifilt,'c') %with champaign, run only ipip1
+    pips = 1:3;
+else % run only fixed, 90% and 99% pipelines 
+    pips = 1:9;
+end
+
 
 if params.ip==7 % source localization is varied 
     %reload data from ip1 to keep them constant and only vary the source
@@ -102,10 +111,10 @@ elseif strcmp(params.ifilt,'c') %champ
     sigma2_total = mean(diag(cov(signal_sensor(:, :)')));
     regu = sigma2_total*0.2;
     sigu = regu*eye(n_sensors);
-    L_backward = permute(L_backward,[1 3 2]);
+    L_perm = permute(L_backward,[1 3 2]);
     
-    [~,~,w] = awsm_champ(signal_sensor(:, :), L_backward(:, :), sigu, 200, 3, 2, 0);    
-    A = real(reshape(w',size(L_backward)));
+    [~,~,w] = awsm_champ(signal_sensor(:, :), L_perm(:, :), sigu, 200, 3, 2, 0);    
+    A = real(reshape(w',size(L_perm)));
 end
 
 t.filter = toc;
@@ -113,9 +122,7 @@ t.filter = toc;
 
 %% Loop over different pipelines
 
-
-%%%%%%%to do: in most of the cases only pips 1 to 9%%%%%%%%%%%%
-for ipip = 1:20
+for ipip = pips
     
     %1 to 6: fixed
     %7: 90%
@@ -126,12 +133,14 @@ for ipip = 1:20
     %12: 99% corrected
     %13 to 20: equal to 1 to 8 but with zscoring
     
-    clear MIM_ MIC_ iCOH_ aCOH_ 
+    fprintf(['Testing pipeline ' num2str(ipip) '\n']) 
     tic
     %% PCA
     
     clear npcs
     signal_roi = [];
+    empty_rois =[];
+    active_rois = [];
     
     %loop over regions
     for aroi = 1:D.nroi
@@ -143,25 +152,35 @@ for ipip = 1:20
         
         if ipip == 9 %baseline: pre-select voxels of true activity
             A_ = A_(:,:,D.sub_ind_roi_region{aroi});
+            
+        elseif ipip == 22 %pick central voxel of each region
+            A_ = A_(:,:,D.ctr_ind_roi_region{aroi});
         end
         
         %number of voxels at the current roi
-        nvoxroi(aroi) = size(A_,3);
-        
+        nvoxroi(aroi) = size(A_,3);        
         A2{aroi} = reshape(A_, [n_sensors, ndim*nvoxroi(aroi)]);
         
         %project sensor signal to voxels at the current roi (aroi)
         signal_source = A2{aroi}' * signal_sensor(:,:);
         
         if ipip > 12 %zscoring pipelines 
-            signal_source = zscore(signal_source)*sqrt(mean(var(signal_source)));
+            signal_source = zscore(signal_source);
         end
         
-        if ~(ipip == 9) && ~(ipip == 10)
+        clear s_
+        if strcmp(params.ifilt,'c') %in case of the champaign filter 
+            %sort out the dims and voxels with zero activity
+            s_ = sum(signal_source,2)>10^-8 | sum(signal_source,2)< -(10^-8) ;
+        else 
+            s_ = logical(ones(size(signal_source,1),1));
+        end
+        
+        if ~(ipip == 9) && ~(ipip == 10) && ipip < 21
             
             %do PCA
             clear signal_roi_ S
-            [signal_roi_,S,~] = svd(double(signal_source)','econ');
+            [signal_roi_,S,~] = svd(double(signal_source(s_,:))','econ');
             
             % variance explained
             vx_ = cumsum(diag(S).^2)./sum(diag(S).^2);
@@ -169,172 +188,183 @@ for ipip = 1:20
             varex = vx_(invx);
             
             %save npcs and variance explained 
-            if ismember(ipip,[7 11])
+            if ismember(ipip,[7 11 19])
                 %npcs are selected in a way that 90% of the variance is preserved
                 npcs(aroi) = min(find(varex> 0.9)); 
                 var_explained=0.9;
-            elseif ismember(ipip,[8 12])
+            elseif ismember(ipip,[8 12 20])
                 %npcs are selected in a way that 99% of the variance is preserved
                 npcs(aroi) = min(find(varex> 0.99));
                 var_explained=0.99;
             elseif ipip <= 6
                 %fixed number of pcs 
                 npcs(aroi) = ipip;
-                var_explained(aroi) = varex(npcs(aroi));
-            elseif ipip >= 13
+                try %may not be possible with the champaign filter
+                    var_explained(aroi) = varex(npcs(aroi));
+                end
+            elseif ipip >= 13 && ipip <= 18
                 %fixed number of pcs 
                 npcs(aroi) = ipip-12;
                 var_explained(aroi) = varex(npcs(aroi));
             end
             
-            %bring signal_roi to the shape of npcs x l_epoch x n_trials
-            signal_roi = cat(1,signal_roi,reshape((signal_roi_(:,1:npcs(aroi))* S(1:npcs, 1:npcs))',[],l_epoch,n_trials));
+            if strcmp(params.ifilt,'c') %champaign filter 
+                %sort out rois with zero activity
+                try
+                    %bring signal_roi to the shape of npcs x l_epoch x n_trials
+                    signal_roi = cat(1,signal_roi,reshape((signal_roi_(:,1:npcs(aroi)))',[],l_epoch,n_trials));
+                    active_rois = [active_rois aroi];
+                catch
+                    empty_rois = [empty_rois aroi];
+                end
+                
+            else
+                %bring signal_roi to the shape of npcs x l_epoch x n_trials
+                signal_roi = cat(1,signal_roi,reshape((signal_roi_(:,1:npcs(aroi)))',[],l_epoch,n_trials));
+            end
             
-        elseif ipip == 9
-            %baseline
+            
+            
+        elseif ipip == 9 || ipip == 22
+            %baseline and central voxel pipeline
             signal_roi = cat(1,signal_roi,reshape(signal_source,[],l_epoch,n_trials));
             npcs(aroi) = 3;
             
         elseif ipip == 10
             %sumVox
             signal_roi{aroi} = reshape(signal_source,[],l_epoch,n_trials);
+            
+        elseif ipip == 21 
+            %sum of activity, then MIM 
+            signal_roi = cat(1,signal_roi, squeeze(mean(reshape(signal_source,ndim, nvoxroi(aroi),l_epoch,n_trials),2)));
+            npcs(aroi) = 3;
         end
     end
     
     
     
-    %% calculate MIM/MIC
+    %% calculate connectivity scores 
    
-    
-    % Calculate mim, mic, iCoh, aCoh and trgc for all pipelines besides sumVox.
-    if ipip ~= 10
+    if ipip ~= 10 && ipip ~= 11 && ipip ~= 12 
         
-        %% calculate indices
-        clear beg_inds end_inds PCA_inds conn output
-        
-        beg_inds = cumsum([1 npcs(1:end-1)]);
-        end_inds = cumsum([npcs]);
-        
-        for iroi = 1:numel(npcs)
-            PCA_inds{iroi} = beg_inds(iroi):end_inds(iroi);
+        if strcmp(params.ifilt,'c') 
+            %no calculations for empty rois 
+            npcs(empty_rois) = [];
         end
         
-        inds = {}; ninds = 0;
-        for iroi = 1:numel(npcs)
-            for jroi = (iroi+1):numel(npcs)
-                inds{ninds+1} = {PCA_inds{iroi}, PCA_inds{jroi}};
-                ninds = ninds + 1;
-            end
-        end
+        % calculate indices
+        clear inds PCA_inds
+        [inds, PCA_inds] = fp_npcs2inds(npcs);
         
-        %% calculate MIM, MIC, TRGC and COH 
-        
+        % calculate MIM, MIC, TRGC and COH        
         output = {'MIM','MIC','TRGC','COH'};
         conn = data2sctrgcmim(signal_roi, fres, 20, 0,0, [], inds, output);
         
-        %% extract measures out of the conn struct
-        iinds = 0;
-        for iroi = 1:D.nroi
-            for jroi = (iroi+1):D.nroi
-                iinds = iinds + 1;
-                MIM_(iroi, jroi) = mean(conn.MIM(filt.band_inds, iinds));
-                MIC_(iroi, jroi) = mean(conn.MIC(filt.band_inds, iinds));
-                DIFFGC_(iroi,jroi) = mean(squeeze(conn.TRGC(filt.band_inds,iinds,1) - conn.TRGC(filt.band_inds,iinds,2)));
-            end
+        % extract measures out of the conn struct
+        [MIM_, MIC_, DIFFGC_, iCOH_, aCOH_] = fp_unwrap_conn(conn,numel(npcs),filt,PCA_inds);
+        
+        if strcmp(params.ifilt,'c')
+            % fill active rois again into the full nroixnroi structure 
+            
+            clear mim mic diffgc 
+            mim = zeros(D.nroi,D.nroi);
+            mim(active_rois,active_rois) = MIM_;
+            MIM_ = mim; 
+            
+            mic = zeros(D.nroi,D.nroi);
+            mic(active_rois,active_rois) = MIC_;
+            MIC_ = mic; 
+            
+            diffgc = zeros(D.nroi,D.nroi);
+            diffgc(active_rois,active_rois) = DIFFGC_;
+            DIFFGC_ = diffgc;
+            
+            icoh = zeros(D.nroi,D.nroi);
+            icoh(active_rois,active_rois) = iCOH_;
+            iCOH_ = icoh; 
+            
+            acoh = zeros(D.nroi,D.nroi);
+            acoh(active_rois,active_rois) = aCOH_;
+            aCOH_ = acoh; 
+
         end
         
-        clear iCOH aCOH
-        for iroi = 1:D.nroi
-            for jroi = 1:D.nroi
-                iCOH_(iroi, jroi) = mean(mean(mean(abs(imag(conn.COH(filt.band_inds, PCA_inds{iroi}, PCA_inds{jroi}))),1), 2), 3);
-                aCOH_(iroi, jroi) = mean(mean(mean(abs(conn.COH(filt.band_inds, PCA_inds{iroi}, PCA_inds{jroi})),1), 2), 3);
-            end
-        end
+    elseif ipip == 10 %sumVox pipeline 
         
+        [MIM_, MIC_] = fp_sumVox_pip(signal_roi, D.nroi, ndim, nvoxroi, fres, filt);
         
-    elseif ipip == 10
+    elseif ipip == 11 || ipip == 12 %normalized 90% and 99% pipeline 
         
-        output = {'MIM','MIC'};
+        % calculate indices
+        clear inds PCA_inds
+        [inds, PCA_inds] = fp_npcs2inds(npcs);
         
-        % For memory purposes, we need to calculate this in a
-        % region-by-region way
-        for oroi = 1:D.nroi-1
-            for uroi = oroi+1:D.nroi
-                clear data npcs beg_inds end_inds PCA_inds mic mim conn 
-                
-                npcs = repmat(ndim,1,nvoxroi(oroi)+nvoxroi(uroi));
-                data = cat(1,signal_roi{oroi}, signal_roi{uroi});
-                
-                beg_inds = cumsum([1 npcs(1:end-1)]);
-                end_inds = cumsum([npcs]);
-                
-                for iroi = 1:numel(npcs)
-                    PCA_inds{iroi} = beg_inds(iroi):end_inds(iroi);
-                end
-                
-                inds = {}; ninds = 0;
-                for iroi = 1:numel(npcs)
-                    for jroi = (iroi+1):numel(npcs)
-                        inds{ninds+1} = {PCA_inds{iroi}, PCA_inds{jroi}};
-                        ninds = ninds + 1;
-                    end
-                end
-                
-                conn = data2sctrgcmim(data, fres, 20, 0,0, [], inds, output);
-                
-                % extract measures out of the conn struct
-                iinds = 0;
-                for ivox = 1:nvoxroi(oroi)+nvoxroi(uroi)-1
-                    for jvox = (ivox+1):nvoxroi(oroi)+nvoxroi(uroi)
-                        iinds = iinds + 1;
-                        mim(ivox, jvox,:) = conn.MIM(:, iinds)';
-                        mic(ivox, jvox,:) = conn.MIC(:, iinds)';
-                    end
-                end
-                
-                MIM_(oroi,uroi) = squeeze(mean(mean(mean(mim(1:nvoxroi(oroi),nvoxroi(oroi):end,filt.band_inds),1),2),3));
-                MIC_(oroi,uroi) = squeeze(mean(mean(mean(mic(1:nvoxroi(oroi),nvoxoig(oroi):end,filt.band_inds),1),2),3));
-                aCOH_=[];
-                iCOH_=[];
-                
-            end
-        end
-        npcs = [];
-        
-        
-    end %ipip == 10
+        %calculate normalized values of MIM and MIC 
+        [MIM_, MIC_, iCOH_, aCOH_] = fp_correct_mim(signal_roi,inds,fres,D,filt,PCA_inds);
+     
+    end 
     
     t.pips(ipip) = toc;
     
-    %% save a few more things 
+    %% save stuff
     
     MIM{ipip} = MIM_;
     MIC{ipip} = MIC_;
     
     if ipip ~= 10
         
-        DIFFGC{ipip} = DIFFGC_;
         aCOH{ipip} = aCOH_;
         iCOH{ipip} = iCOH_;
         
-        if ipip ~= 9
+        if ipip ~= 11 && ipip ~= 12
+            DIFFGC{ipip} = DIFFGC_;
+        end
+        
+        if ipip ~= 9 && ipip < 21
             to_save{ipip}.npcs = npcs;
             to_save{ipip}.varex = var_explained;
 
             nvoxroi_all = nvoxroi'*nvoxroi;
             nvoxroi_all = nvoxroi_all(:);
-            to_save{ipip}.corr_voxmim = corr(nvoxroi_all,MIM_(:));
-            to_save{ipip}.corr_voxmic = corr(nvoxroi_all ,MIC_(:));
-            to_save{ipip}.corr_voxicoh = corr(nvoxroi_all,iCOH_(:));
-            to_save{ipip}.corr_voxacoh = corr(nvoxroi_all,aCOH_(:));
-            to_save{ipip}.corr_voxnpcs = corr(nvoxroi', npcs');
+            corr_voxmim(ipip) = corr(nvoxroi_all,MIM_(:));
+            corr_voxmic(ipip) = corr(nvoxroi_all ,MIC_(:));
+            corr_voxicoh(ipip) = corr(nvoxroi_all,iCOH_(:));
+            corr_voxacoh(ipip) = corr(nvoxroi_all,aCOH_(:));
+            corr_voxnpcs(ipip) = corr(nvoxroi', npcs');
         end
         
     end
     
+    
+    
+    %% Evaluate 
+    
+    [mrr_mic(ipip), pr_mic(ipip),hk_mic(ipip)] = fp_mrr_hk(MIC_,iroi_seed,iroi_tar,1);        
+    [mrr_mim(ipip), pr_mim(ipip),hk_mim(ipip)] = fp_mrr_hk(MIM_,iroi_seed,iroi_tar,1);
+    
+    if ipip ~= 10 
+        [mrr_aCoh(ipip), pr_aCoh(ipip),hk_aCoh(ipip)] = fp_mrr_hk(aCOH_,iroi_seed,iroi_tar,1);
+        [mrr_iCoh(ipip), pr_iCoh(ipip),hk_iCoh(ipip)] = fp_mrr_hk(iCOH_,iroi_seed,iroi_tar,1);
+        
+        if ipip ~= 11 && ipip ~= 12 
+            %absolute value of gc and only triu is considered. Metric neglects
+            %the direction of the interaction 
+            [mrr_absgc(ipip), pr_absgc(ipip),hk_absgc(ipip)] = fp_mrr_hk(abs(DIFFGC_),iroi_seed,iroi_tar,1);
+
+            %only positive part of gc is submitted and the whole matrix is
+            %considered. Metric that is strongly influenced by the direction of
+            %the effect 
+            clear pos_diffgc
+            pos_diffgc = DIFFGC_; 
+            pos_diffgc(pos_diffgc< 0) = 0;
+            [mrr_posgc(ipip), pr_posgc(ipip),hk_posgc(ipip)] = fp_mrr_hk(pos_diffgc,iroi_seed,iroi_tar,0);
+        end
+        
+    end
+
+
+    
     clear MIM_ MIC_ DIFFGC_ aCOH_ iCOH_ 
-
-
     
     
     
@@ -342,16 +372,17 @@ end %pips
 
 %% Saving 
 fprintf('Saving... \n')
+%save all
 outname = sprintf('%smim_%s.mat',DIROUT,params.logname);
 save(outname,'-v7.3')
 
-
-
-
-
-
-
-%%
-
-% correct, ALI, order pipelines 
+%save only evaluation parameters
+outname1 = sprintf('%smrr_%s.mat',DIROUT,params.logname);
+save(outname1,...
+    'mrr_mic','pr_mic','hk_mic',...
+    'mrr_mim','pr_mim','hk_mim',...
+    'mrr_aCoh','pr_aCoh','hk_aCoh',...
+    'mrr_iCoh','pr_iCoh','hk_iCoh',...
+    'mrr_absgc','pr_absgc','hk_absgc',...
+    'mrr_posgc','pr_posgc','hk_posgc','-v7.3')
 
