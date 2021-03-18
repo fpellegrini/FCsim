d3154f1a130f281e426409c5b311434270740ff9
fpellegrini <franziskapellegrini@web.de>
2021-03-18 19:15:19 +0100

    adapted the function to new simulation structure

--- fp_correct_mim.m
+++ fp_correct_mim.m
@@ -1,112 +1,49 @@
-function [mic, mim, to_save, mean_icoh,mean_acoh] = fp_correct_mim(A,signal_sensor, fqA, nfqA, D, ihemi, mic, mim, mean_icoh, mean_acoh, to_save,fres)
+function [MIM, MIC, iCOH, aCOH] = fp_correct_mim(signal_roi,inds,fres,D,filt,PCA_inds)
 
-nit = 50; 
-n_trials = size(signal_sensor,3); 
-zs=1;
+nit = 50;
+[~,~, n_trials] = size(signal_roi);
 
+%% true MIM and MIC
+output = {'MIM','MIC'};
+conn = data2sctrgcmim(signal_roi, fres, 20, 0,0, [], inds, output);
+% extract measures out of the conn struct
+[MIM_t, MIC_t, ~, iCOH_t, aCOH_t] = fp_unwrap_conn(conn,D.nroi,filt,PCA_inds);
 
-for iit = 1:nit 
+%% null distribution
+
+for iit = 1:nit
     
-    fprintf('Iteration %d... \n',iit)
-    id_trials_1 = 1:n_trials;
-    id_trials_2 = randperm(n_trials);
-    id_meg_chan = 1:size(signal_sensor,1);
-    nmeg = numel(id_meg_chan);
+    %shuffle trials
+    shuf_inds = randperm(n_trials);
+    sig_s = signal_roi(:,:,shuf_inds);
     
-    %cross spectrum
-    fprintf('Calculating cross spectrum... \n')
-    CS = fp_tsdata_to_cpsd(signal_sensor,fres,'WELCH',...
-        [id_meg_chan], [id_meg_chan], id_trials_1, id_trials_2);
-    CS(:,:,1)=[];
-    nfreq = size(CS,3);
-    
-    [mic_max_shuf(:,:,:,iit), mim_max_shuf(:,:,:,iit), ~, ...
-        mean_coh_max_shuf(:,:,:,iit),mean_abscoh_max_shuf(:,:,:,iit),~] = ...
-        fp_get_mim(A,CS,fqA,nfqA, D,ihemi,'max',zs,[]);
-    
-    [mic90_shuf(:,:,:,iit), mim90_shuf(:,:,:,iit), ~, ...
-        mean_coh90_shuf(:,:,:,iit), mean_abscoh90_shuf(:,:,:,iit),~ ] = ...
-        fp_get_mim(A,CS,fqA,nfqA, D,ihemi,'percent',zs,[]);
-    
+    %calculate MIM and MIC
+    clear conn
+    output = {'MIM','MIC','COH'};
+    conn = data2sctrgcmim(sig_s, fres, 20, 0,0, [], inds, output);        
+    % extract measures out of the conn struct
+    [MIM_s(:,:,iit), MIC_s(:,:,iit), ~, iCOH_s(:,:,iit), aCOH_s(:,:,iit)] = fp_unwrap_conn(conn,D.nroi,filt,PCA_inds);    
 end
 
-%%
+%% Normalization
 
-clear m_max s_max
-m_max = mean(mic_max_shuf,4); 
-s_max = std(mic_max_shuf,0,4); 
-mic.max_corrected = (mic.max - m_max)./s_max;
+clear m_ s_
+m_ = mean(MIC_s,3);
+s_ = std(MIC_s,0,3);
+MIC = (MIC_t - m_)./s_;
 
-clear m_max s_max
-m_max = mean(mim_max_shuf,4); 
-s_max = std(mim_max_shuf,0,4); 
-mim.max_corrected = (mim.max - m_max)./s_max;
+clear m_ s_
+m_ = mean(MIM_s,3);
+s_ = std(MIM_s,0,3);
+MIM = (MIM_t - m_)./s_;
 
-clear m_max s_max
-m_max = mean(mean_coh_max_shuf,4); 
-s_max = std(mean_coh_max_shuf,0,4); 
-mean_icoh.max_corrected = (mean_icoh.max - m_max)./s_max;
+clear m_ s_
+m_ = mean(iCOH_s,3);
+s_ = std(iCOH_s,0,3);
+iCOH = (iCOH_t - m_)./s_;
 
-clear m_max s_max
-m_max = mean(mean_abscoh_max_shuf,4); 
-s_max = std(mean_abscoh_max_shuf,0,4); 
-mean_acoh.max_corrected = (mean_acoh.max - m_max)./s_max;
-
-clear m90 s90
-m90 = mean(mic90_shuf,4); 
-s90 = std(mic90_shuf,0,4); 
-mic.percent_corrected = (mic.percent - m90)./s90; 
-
-clear m90 s90
-m90 = mean(mim90_shuf,4); 
-s90 = std(mim90_shuf,0,4); 
-mim.percent_corrected = (mim.percent - m90)./s90; 
-
-clear m90 s90
-m90 = mean(mean_coh90_shuf,4); 
-s90 = std(mean_coh90_shuf,0,4); 
-mean_icoh.percent_corrected = (mean_icoh.percent - m90)./s90; 
-
-clear m90 s90
-m90 = mean(mean_abscoh90_shuf,4); 
-s90 = std(mean_abscoh90_shuf,0,4); 
-mean_acoh.percent_corrected = (mean_acoh.percent - m90)./s90; 
-
-to_save.max_corrected.mim_shuf = mim_max_shuf; 
-to_save.max_corrected.mic_shuf = mic_max_shuf; 
-to_save.max_corrected.mean_icoh_shuf = mean_coh_max_shuf; 
-to_save.max_corrected.mean_acoh_shuf = mean_abscoh_max_shuf; 
-
-to_save.percent_corrected.mim_shuf = mim90_shuf; 
-to_save.percent_corrected.mic_shuf = mic90_shuf; 
-to_save.percent_corrected.mean_icoh_shuf = mean_coh90_shuf; 
-to_save.percent_corrected.mean_acoh_shuf = mean_abscoh90_shuf; 
-
-%%
-nvoxroi_all = to_save.nvoxroi'* to_save.nvoxroi;
-nvoxroi_all = nvoxroi_all(:);
-
-c1 = sum(mim.max_corrected,3);
-c2 = sum(mic.max_corrected,3);
-c3 = sum(mean_icoh.max_corrected,3);
-c4 = sum(mean_acoh.max_corrected,3);
-to_save.max_corrected.corr_voxmim = corr(nvoxroi_all,c1(:));
-to_save.max_corrected.corr_voxmic = corr(nvoxroi_all ,c2(:));
-to_save.max_corrected.corr_voxnpcs = corr(to_save.nvoxroi', to_save.max.npcs');
-to_save.max_corrected.corr_voxmeancoh = corr(nvoxroi_all,c3(:));
-to_save.max_corrected.corr_voxmeanabscoh = corr(nvoxroi_all,c4(:));
-
-c1 = sum(mim.percent_corrected,3);
-c2 = sum(mic.percent_corrected,3);
-c3 = sum(mean_icoh.percent_corrected,3);
-c4 = sum(mean_acoh.percent_corrected,3);
-to_save.percent_corrected.corr_voxmim = corr(nvoxroi_all,c1(:));
-to_save.percent_corrected.corr_voxmic = corr(nvoxroi_all,c2(:));
-to_save.percent_corrected.corr_voxnpcs = corr(to_save.nvoxroi', to_save.percent.npcs');
-to_save.percent_corrected.corr_voxmeancoh = corr(nvoxroi_all,c3(:));
-to_save.percent_corrected.corr_voxmeanabscoh = corr(nvoxroi_all,c4(:));
-
-
-
-
+clear m_ s_
+m_ = mean(aCOH_s,3);
+s_ = std(aCOH_s,0,3);
+aCOH = (aCOH_t - m_)./s_;
 
