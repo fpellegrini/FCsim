9fd2c776b6979b04bc90835fc276c34b2e0516b0
fpellegrini <franziskapellegrini@web.de>
2021-04-08 16:22:01 +0200

    implemented em evaluation measures

--- fp_mrr_hk.m
+++ fp_mrr_hk.m
@@ -1,18 +1,21 @@
-function [mrr, pr,hk] = fp_mrr_hk(cc,iroi_seed,iroi_tar,half_m)
+function [mrr, pr,hk,em1,em2,em3] = fp_mrr_hk(cc,iroi_seed,iroi_tar,half_m)
 
 if norm(iroi_seed(:)- iroi_tar(:),'fro')==0
     mrr = 0;
-    pr=0;
+    pr = 0;
     hk = 0; 
+    em1 = 0;
+    em2 = 0;
+    em3 = 0;
 else
 
     nints = numel(iroi_seed);
     nroi = size(cc,1);
     
     if half_m 
-        inds = find(triu(ones(nroi),1));
+        ind_upper = find(triu(ones(nroi),1));
     else
-        inds = find(ones(nroi));
+        ind_upper = find(ones(nroi));
     end
 
     label= zeros(nroi,nroi);
@@ -23,16 +26,17 @@ else
         end
     end
     lab = label(:);
-    tr = find(lab(inds)==1);
-    k=10;
+    tr = find(lab(ind_upper)==1);
+    
+    %% MRR
     for ii = 1:nints
        perfectSkill(ii) = 1/ii;
-       noSkill(ii) = 1/(numel(lab(inds))-ii+1);
+       noSkill(ii) = 1/(numel(lab(ind_upper))-ii+1);
     end
     perfectSkill = sum(perfectSkill)/nints;
     noSkill = sum(noSkill)/nints;
 
-    [ss, idx] = sort(cc(inds),'descend');
+    [ss, idx] = sort(cc(ind_upper),'descend');
 
     for it = 1:numel(tr)
         r1(it) = find(idx==tr(it));
@@ -41,13 +45,52 @@ else
     mrr = sum(1./r1)/nints;
     mrr = (mrr-noSkill)/(perfectSkill-noSkill);
     
+    %% hits @ k 
+    k=10;
     hk = sum(r1<=k)/nints;
     
+    %% area under the precision-recall curve 
     if any(r1>nints)
-        [~,~,~,pr]=perfcurve(lab(inds),cc(inds),1,'XCrit','reca','YCrit','prec');
+        [~,~,~,pr]=perfcurve(lab(ind_upper),cc(ind_upper),1,'XCrit','reca','YCrit','prec');
         
     else %perfect skill
         pr=1;
+    end
+    
+    %% moving earth distance in 3 variants
+    try
+        load('processed_bs_wzb_90_2000/bs_results.mat')
+        iatl = 3; % DK atlas
+        neighbor_thresh = 10; % 10mm vicinity defines neighborhood between regions
+        [conndist, ~,~, ~] = get_ROI_dist2(cortex, iatl, neighbor_thresh);
+
+        d1 = label(ind_upper);
+        d1 = d1 ./ sum(d1(:));
+
+        d2 = cc(ind_upper);
+        d2 = d2 - min(d2);
+
+        d2_ = d2;
+        d2_ = d2_ ./ sum(d2_(:));
+        em1 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+
+        d2_ = d2;
+        d2_(d2_ < prctile(d2_, 95)) = 0;
+        d2_ = d2_ ./ sum(d2_(:));
+        em2 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+
+        d2_ = d2;
+        [so, ~] = sort(d2_, 'descend');
+        d2_(d2_ < so(numel(iroi_seed))) = 0;
+        d2_ = d2_ ./ sum(d2_(:));
+        em3 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+        
+    catch 
+        
+        em1 = 0; 
+        em2 = 0; 
+        em3 = 0; 
+        
     end
     
     
