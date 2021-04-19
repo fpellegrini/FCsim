2ca6159ed769e21aaa0873a7c7a79b1590947cf8
fpellegrini <franziskapellegrini@web.de>
2021-04-19 18:22:38 +0200

    added changes to em that Stefan suggested

--- fp_add_em.m
+++ fp_add_em.m
@@ -5,7 +5,7 @@ DIRIN = './mim_sim3/';
 if ~exist(DIRIN);mkdir(DIRIN); end
 
 %%
-for iname = [5: 15]
+for iname = [1: 15]
     
     iname
     
@@ -46,12 +46,14 @@ for iname = [5: 15]
                 ifilt = 'e';
             case 15
                 ifilt = 'c';
+            case 16 
+                ifilt = 'cr';
         end
     end
     
     %%
     for iit= 1:nit
-        
+        tic
         try
             iit
             clearvars -except iInt iReg isnr iss ilag ifilt iit nit iname DIRIN
@@ -67,6 +69,8 @@ for iname = [5: 15]
                 MIM_ = MIM{ipip};
                 aCOH_ = aCOH{ipip};
                 iCOH_ = iCOH{ipip};
+                DIFFGC_ = DIFFGC{ipip}; 
+                
                 
                 
                 [mrr_mic(ipip), pr_mic(ipip),hk_mic(ipip),em1_mic(ipip),em2_mic(ipip),em3_mic(ipip)] = fp_mrr_hk(MIC_,iroi_seed,iroi_tar,1);
@@ -76,19 +80,31 @@ for iname = [5: 15]
                     [mrr_aCoh(ipip), pr_aCoh(ipip),hk_aCoh(ipip),em1_aCoh(ipip),em2_aCoh(ipip),em3_aCoh(ipip)] = fp_mrr_hk(aCOH_,iroi_seed,iroi_tar,1);
                     [mrr_iCoh(ipip), pr_iCoh(ipip),hk_iCoh(ipip),em1_iCoh(ipip),em2_iCoh(ipip),em3_iCoh(ipip)] = fp_mrr_hk(iCOH_,iroi_seed,iroi_tar,1);
                     
-                    %                 if ipip ~= 11 && ipip ~= 12  && ipip < 21
-                    %                     %absolute value of gc and only triu is considered. Metric neglects
-                    %                     %the direction of the interaction
-                    %                     [mrr_absgc(ipip), pr_absgc(ipip),hk_absgc(ipip),em1_absgc(ipip),em2_absgc(ipip),em3_absgc(ipip)] = fp_mrr_hk(abs(DIFFGC_),iroi_seed,iroi_tar,1);
-                    %
-                    %                     %only positive part of gc is submitted and the whole matrix is
-                    %                     %considered. Metric that is strongly influenced by the direction of
-                    %                     %the effect
-                    %                     clear pos_diffgc
-                    %                     pos_diffgc = DIFFGC_;
-                    %                     pos_diffgc(pos_diffgc< 0) = 0;
-                    %                     [mrr_posgc(ipip), pr_posgc(ipip),hk_posgc(ipip),em1_posgc(ipip),em2_posgc(ipip),em3_posgc(ipip)] = fp_mrr_hk(pos_diffgc,iroi_seed,iroi_tar,0);
-                    %                 end
+                    if ipip ~= 11 && ipip ~= 12  && ipip < 21
+                        %absolute value of gc and only triu is considered. Metric neglects
+                        %the direction of the interaction
+                        [mrr_absgc(ipip), pr_absgc(ipip),hk_absgc(ipip),...
+                            em1_absgc(ipip),em2_absgc(ipip),em3_absgc(ipip)] ...
+                            = fp_mrr_hk(abs(DIFFGC_),iroi_seed,iroi_tar,1);
+                        
+                        %only positive part of gc is submitted and the whole matrix is
+                        %considered. Metric that is strongly influenced by the direction of
+                        %the effect
+                        clear pos_diffgc
+                        pos_diffgc = DIFFGC_;
+                        pos_diffgc(pos_diffgc< 0) = 0;
+                        [mrr_posgc(ipip), pr_posgc(ipip),hk_posgc(ipip),...
+                            em1_posgc(ipip),em2_posgc(ipip),em3_posgc(ipip)]...
+                            = fp_mrr_hk(pos_diffgc,iroi_seed,iroi_tar,0);
+                        
+                        %wrong directions 
+                        clear pos_diffgc_w
+                        pos_diffgc_w = -DIFFGC_;
+                        pos_diffgc_w(pos_diffgc_w < 0) = 0;
+                        [mrr_posgc_w(ipip), pr_posgc_w(ipip),hk_posgc_w(ipip),...
+                            em1_posgc_w(ipip),em2_posgc_w(ipip),em3_posgc_w(ipip)]...
+                            = fp_mrr_hk(pos_diffgc_w,iroi_seed,iroi_tar,0);
+                    end
                     
                 end
             end
@@ -100,11 +116,13 @@ for iname = [5: 15]
                 'mrr_mim','pr_mim','hk_mim','em1_mim','em2_mim','em3_mim',...
                 'mrr_aCoh','pr_aCoh','hk_aCoh','em1_aCoh','em2_aCoh','em3_aCoh',...
                 'mrr_iCoh','pr_iCoh','hk_iCoh','em1_iCoh','em2_iCoh','em3_iCoh',...
-                'mrr_absgc','pr_absgc','hk_absgc',...
-                'mrr_posgc','pr_posgc','hk_posgc',...
+                'mrr_absgc','pr_absgc','hk_absgc','em1_absgc','em2_absgc','em3_absgc',...
+                'mrr_posgc','pr_posgc','hk_posgc','em1_posgc','em2_posgc','em3_posgc',...
+                'mrr_posgc_w','pr_posgc_w','hk_posgc_w','em1_posgc_w','em2_posgc_w','em3_posgc_w',...
                 '-v7.3')
             
             
+            toc
         end
     end
 end
--- fp_mrr_hk.m
+++ fp_mrr_hk.m
@@ -13,9 +13,9 @@ else
     nroi = size(cc,1);
     
     if half_m 
-        ind_upper = find(triu(ones(nroi),1));
+        ind_ = find(triu(ones(nroi),1));
     else
-        ind_upper = find(ones(nroi));
+        ind_ = find(ones(nroi));
     end
 
     label= zeros(nroi,nroi);
@@ -26,17 +26,17 @@ else
         end
     end
     lab = label(:);
-    tr = find(lab(ind_upper)==1);
+    tr = find(lab(ind_)==1);
     
     %% MRR
     for ii = 1:nints
        perfectSkill(ii) = 1/ii;
-       noSkill(ii) = 1/(numel(lab(ind_upper))-ii+1);
+       noSkill(ii) = 1/(numel(lab(ind_))-ii+1);
     end
     perfectSkill = sum(perfectSkill)/nints;
     noSkill = sum(noSkill)/nints;
 
-    [ss, idx] = sort(cc(ind_upper),'descend');
+    [ss, idx] = sort(cc(ind_),'descend');
 
     for it = 1:numel(tr)
         r1(it) = find(idx==tr(it));
@@ -51,7 +51,7 @@ else
     
     %% area under the precision-recall curve 
     if any(r1>nints)
-        [~,~,~,pr]=perfcurve(lab(ind_upper),cc(ind_upper),1,'XCrit','reca','YCrit','prec');
+        [~,~,~,pr]=perfcurve(lab(ind_),cc(ind_),1,'XCrit','reca','YCrit','prec');
         
     else %perfect skill
         pr=1;
@@ -62,30 +62,31 @@ else
         load('processed_bs_wzb_90_2000/bs_results.mat')
         iatl = 3; % DK atlas
         neighbor_thresh = 10; % 10mm vicinity defines neighborhood between regions
-        [conndist, ~,~, ~] = get_ROI_dist2(cortex, iatl, neighbor_thresh);
+%         [conndist, ~,~, ~] = get_ROI_dist2(cortex, iatl, neighbor_thresh);
+        [conndist_full, ~, ~, ~] = get_ROI_dist_full(cortex, iatl, neighbor_thresh);
 
-        d1 = label(ind_upper);
+        d1 = label(:); % takes always the full matrix, not only upper half 
         d1 = d1 ./ sum(d1(:));
 
-        d2 = cc(ind_upper);
+        d2 = cc(:);
         d2 = d2 - min(d2);
 
         d2_ = d2;
         d2_ = d2_ ./ sum(d2_(:));
-        em1 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+        em1 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist_full);
 
         d2_ = d2;
         d2_(d2_ < prctile(d2_, 95)) = 0;
         d2_ = d2_ ./ sum(d2_(:));
-        em2 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+        em2 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist_full);
 
         d2_ = d2;
         [so, ~] = sort(d2_, 'descend');
         d2_(d2_ < so(numel(iroi_seed))) = 0;
         d2_ = d2_ ./ sum(d2_(:));
-        em3 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist);
+        em3 = 1 - emd_hat_gd_metric_mex(d1, d2_, conndist_full);
         
-    catch 
+    catch %that not the full simulation stops when compilation of mex files fail 
         
         em1 = 0; 
         em2 = 0; 
