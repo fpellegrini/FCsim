dedbb76d71a367e44d316a5cec2a7773935b1c55
fpellegrini <franziskapellegrini@web.de>
2021-05-06 11:56:39 +0200

    corrected analysis according to Stefan's suggestions

--- fp_lag_check.m
+++ fp_lag_check.m
@@ -1,5 +1,12 @@
 function fp_lag_check
 
+fs= 100; 
+fres = fs; 
+frqs = sfreqs(fres, fs); % freqs in Hz
+
+Nmin = 3; % length of recording in minutes
+N = Nmin*60*fs;
+
 iband = [8 12]; % frequency band of interaction in Hz
 band_inds = find(frqs >= iband(1) & frqs <= iband(2)); % indices of interacting frequencies
 
@@ -8,26 +15,58 @@ band_inds = find(frqs >= iband(1) & frqs <= iband(2)); % indices of interacting 
 
 s1 = randn(N, 1);
 s1 = filtfilt(bband, aband, s1);
+Lepo = 2*fres;
+noise = randn(N,2);
 
-for ilag = 1:80
+%%
+il = 1;
+lags=1:8;
+for ilag = lags
 
     s2 = circshift(squeeze(s1), ilag);
-    x1 = hilbert(s1); 
-    x2 = hilbert(s2); 
+    x1 = cat(2,s1,s2);
+    clear x
+    x = (0.9*x1 + 0.1*noise)';
     
-    x3 = abs(imag(x1)-imag(x2));
-    x3_(ilag) = mean(x3); 
+    x = reshape(x,2,Lepo,[]);
+    conn = data2sctrgcmim(x, fres, 20, 0,0, [], [], {'CS'},0);
+
+    a(:,1,il) = angle(conn.CS(:,1,2));
+    a(:,2,il) = angle(conn.CS(:,2,1));
     
-%     plot(x3) 
-%     hold on 
+    il = il+1;
 
 end 
 
-plot(1:80,x3_)
-xlabel('lag size in samples') 
-ylabel('phase difference')
-grid on 
-title('signals filtered in alpha band (8-12 Hz)')
+%%
+figure
+for ilag = 1:8
+    sp(ilag) = subplot(2,4,ilag);
+    plot(frqs,squeeze(a(:,1,ilag)))
+    
+    rectX = iband;
+    rectY = ylim([sp(ilag)]);
+    pch = patch(sp(ilag), rectX([1 2 2 1]) , rectY([1 1 2 2]), 'r', ...
+        'EdgeColor', 'none', 'FaceAlpha', 0.1);
+    
+    title(['lag ' num2str(lags(ilag))])
+    xlabel('Freqs')
+end
+
+%% same but negative 
+figure
+for ilag = 1:8
+    sp(ilag) = subplot(2,4,ilag);
+    plot(frqs,squeeze(a(:,2,ilag)))
+    
+    rectX = iband;
+    rectY = ylim([sp(ilag)]);
+    pch = patch(sp(ilag), rectX([1 2 2 1]) , rectY([1 1 2 2]), 'r', ...
+        'EdgeColor', 'none', 'FaceAlpha', 0.1);
+    
+    title(['lag ' num2str(lags(ilag))])
+    xlabel('Freqs')
+end
 %%
 
 
@@ -51,7 +90,6 @@ for ib = 1:length(frqs)-2
     
     
     s1 = filtfilt(bband, aband, s1_save);
-
 
     s2 = circshift(squeeze(s1), ilag);
     x1 = hilbert(s1); 
