8bb8a8b27dc01d07c3e0b8c6096d4e7753421224
fpellegrini <franziskapellegrini@web.de>
2021-06-01 11:57:12 +0200

    minor change

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -5,7 +5,7 @@ cd ~/matlab/fp/meg_lfp/
 nit = 100;
 varyParam = [1:7];%defaults, snr, interactions, everything else 
 
-for ip = 4 %1,2, 7 
+for ip = 3 %1,2,3,4, 7 
     mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60)
 end
