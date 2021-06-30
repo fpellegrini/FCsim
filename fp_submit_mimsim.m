829d0b42d893a6fb1f8a991847e345be8ce227a3
fpellegrini <franziskapellegrini@web.de>
2021-06-30 13:22:02 +0200

    added ssd pipeline (ip 8)

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -3,9 +3,9 @@
 fp_addpath
 cd ~/matlab/fp/meg_lfp/
 nit = 100;
-varyParam = [1:7];%defaults, snr, interactions, everything else 
+varyParam = [1:8];%defaults, snr, interactions, everything else 
 
-for ip = 6 %1,2,3,4,5,6, 7 
+for ip = 8 %1,2,3,4,5,6, 7 
     mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60)
 end
