7aa5c22527c8e35f471aa12192e1ad2085bfc395
fpellegrini <franziskapellegrini@web.de>
2022-02-23 11:56:22 +0100

    ip3

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -5,7 +5,7 @@ cd ~/matlab/fp/meg_lfp/
 nit = 100;
 varyParam = [1:5 7]; 
 
-for ip = varyParam(1:2) %1 2
+for ip = varyParam(3) %1 2 3
     mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*10)
 end
