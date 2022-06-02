239c47dd21d0c9a52898f7c6d410a6ce7a9be46a
fpellegrini <franziskapellegrini@web.de>
2022-06-02 14:35:16 +0200

    comment

--- fp_addpath.m
+++ fp_addpath.m
@@ -4,7 +4,14 @@ restoredefaultpath
 rehash toolboxcache
 
 addpath(genpath('/home/bbci/data/haufe/Franziska'))
-addpath(genpath('~/matlab/'))
+addpath(genpath('~/matlab/fp/'))
+addpath(genpath('~/matlab/matgrid/'))
+addpath(genpath('~/matlab/libs/Daniele_ARMA/'))
+addpath(genpath('~/matlab/libs/haufe/'))
+addpath(genpath('~/matlab/libs/nolte/'))
+addpath(genpath('~/matlab/libs/mvgc_v1.0/'))
+addpath(genpath('~/matlab/libs/pac_lib/'))
+addpath('~/matlab/libs/eeglab-develop/functions/sigprocfunc/')
 % spm eeg
 % close all 
 cd '/home/bbci/data/haufe/Franziska/data/'
