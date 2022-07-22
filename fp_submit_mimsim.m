c309fa4768d6c182aafe280777ce139f98da5b27
fpellegrini <franziskapellegrini@web.de>
2022-07-22 13:09:59 +0200

    submit again correlated sources setup

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -2,10 +2,10 @@
 
 fp_addpath
 cd ~/matlab/fp/meg_lfp/
-nit = 100;
-varyParam = [1:5 7 9]; 
+nit = 100; %100 iterations per experiment
+varyParam = [1:5 7 9]; %different experimental setups 
 
-for ip = [9] 
+for ip = 9 
     mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*10)
 end
