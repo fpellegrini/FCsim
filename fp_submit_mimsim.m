86f22d72af28239e8799af62630be0ad1911ba14
fpellegrini <franziskapellegrini@web.de>
2022-09-28 15:26:41 +0200

    minor changes

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -5,7 +5,7 @@ cd ~/matlab/fp/meg_lfp/
 nit = 100; %100 iterations per experiment
 varyParam = [1:5 7 9]; %different experimental setups 
 
-for ip = 12 %varyParam 
+for ip = varyParam 
     mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*10)
 end
