d8dab424792691fa42b213f405ba7c33ee72bbd1
fpellegrini <franziskapellegrini@web.de>
2022-10-04 13:11:03 +0200

    add Ali's code

--- fp_addpath.m
+++ fp_addpath.m
@@ -10,6 +10,7 @@ addpath(genpath('~/matlab/libs/Daniele_ARMA/'))
 addpath(genpath('~/matlab/libs/haufe/'))
 addpath(genpath('~/matlab/libs/nolte/'))
 addpath(genpath('~/matlab/libs/mvgc_v1.0/'))
+addpath(genpath('~/matlab/libs/Ali/'))
 addpath(genpath('~/matlab/libs/pac_lib/'))
 addpath('~/matlab/libs/eeglab-develop/functions/sigprocfunc/')
 % spm eeg
