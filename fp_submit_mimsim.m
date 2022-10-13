e4a46f137585f93e2219f667b0774cf0971ede8c
fpellegrini <franziskapellegrini@web.de>
2022-10-13 09:12:50 +0200

    test necessary functions

--- fp_submit_mimsim.m
+++ fp_submit_mimsim.m
@@ -5,7 +5,7 @@ cd ~/matlab/fp/meg_lfp/
 nit = 100; %100 iterations per experiment
 varyParam = [1:5 7 9]; %different experimental setups 
 
-for ip = 7%varyParam 
-    mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
+for ip = varyParam 
+    mgsub({},@fp_eval_mim_struct_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 101' ]) %1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*10)
 end
