a58e8c4b0e5b377dcb68271ad2bd0f81c83923e1
fpellegrini <franziskapellegrini@web.de>
2022-12-13 17:37:05 +0100

    minor  change

--- fp_addpath.m
+++ fp_addpath.m
@@ -4,6 +4,7 @@ addpath(genpath('/home/bbci/data/haufe/Franziska'))
 addpath(genpath('~/matlab/fp/'))
 addpath(genpath('~/matlab/matgrid/'))
 addpath(genpath('~/matlab/libs/mvgc_v1.0/'))
+addpath(genpath('~/matlab/libs/Daniele_ARMA/'))
 addpath(genpath('~/matlab/libs/Ali/'))
 
 cd '/home/bbci/data/haufe/Franziska/data/'
