a81f82708742fe598075781f89637e12b55d93c9
fpellegrini <franziskapellegrini@web.de>
2022-12-13 17:56:10 +0100

    back to normal

--- fp_eval_mim_struct_sim.m
+++ fp_eval_mim_struct_sim.m
@@ -22,9 +22,8 @@ if ~exist(DIRLOG); mkdir(DIRLOG); end
 
 %%
 %iteration number equals cluster job number 
-% iit = str2num(getenv('SGE_TASK_ID'));
-% rng(iit)
-iit =1; 
+iit = str2num(getenv('SGE_TASK_ID'));
+rng(iit)
 
 clear nInteractions nRegionInts SNR noise_mix nlag filtertype hemisym
 
